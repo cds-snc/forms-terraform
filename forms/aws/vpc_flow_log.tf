d5ab47f937c5cb0dbedba99c23a29079d1670599
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-12-11 11:19:06 -0500

    Fix typo in vpc flow logs

--- forms/aws/vpc_flow_log.tf
+++ forms/aws/vpc_flow_log.tf
@@ -2,7 +2,7 @@ resource "aws_flow_log" "vpc_flow_logs" {
   iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
   log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
   traffic_type    = "ALL"
-  vpc_id          = aws_vpc.covidportal.id
+  vpc_id          = aws_vpc.forms.id
 }
 
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
