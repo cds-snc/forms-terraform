9c2ae382e0a9d9725bd08ae82f38a9f4174bd95f
brdunfield <bethany.dunfield@cds-snc.ca>
2021-05-10 13:43:12 -0600

    Revert "add region env var"
    This reverts commit f76b62c25eafdb2782c1531493981544cdf748d6.

--- forms/aws/lambda.tf
+++ forms/aws/lambda.tf
@@ -182,14 +182,10 @@ resource "aws_lambda_function" "templates" {
   handler       = "templates.handler"
 
   source_code_hash = data.archive_file.submission_main.output_base64sha256
-  runtime = "nodejs14.x"
+
   layers  = [aws_lambda_layer_version.templates_lib.arn]
 
-  environment {
-    variables = {
-      REGION  = var.region
-    }
-  }
+  runtime = "nodejs14.x"
 }
 
 resource "aws_lambda_layer_version" "templates_lib" {
