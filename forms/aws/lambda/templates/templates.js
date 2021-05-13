ac2f637dd2bc8833bea77070f551bb3f73948e4f
brdunfield <bethany.dunfield@cds-snc.ca>
2021-05-13 10:41:53 -0600

    update lambda to support inserting and getting

--- forms/aws/lambda/templates/templates.js
+++ forms/aws/lambda/templates/templates.js
@@ -1,33 +1,48 @@
-const { RDSDataClient, BatchExecuteStatementCommand } = require("@aws-sdk/client-rds-data")
+const { RDSDataClient, ExecuteStatementCommand } = require("@aws-sdk/client-rds-data")
 const REGION = process.env.REGION;
 
 exports.handler = async function (event) {
   const dbClient = new RDSDataClient({region: REGION});
-  //if (event.httpMethod == "POST") {
-    // For now querystring - will probably make this a file upload on the body
-    const jsonConfig = event.queryStringParameters.json_config;
+  const method = event.method;
 
-    // TODO - INSERT vs UPDATE
+  let SQL,
+      parameters;
+  switch (method) {
+    case "INSERT":
+      SQL = "INSERT INTO Templates (json_config) VALUES :json_config";
+      parameters = [
+        {
+          name: "json_config",
+          typeHint: "JSON",
+          value: {
+            stringValue: JSON.stringify(event.json_config)
+          }
+        }
+      ];
+      break;
+    case "GET":
+      SQL = "SELECT * FROM Templates";
+      break;
+  }
 
-    const SQL = "INSERT INTO Templates (json_config) VALUES (" + jsonConfig + ")"
-    
-    const params = {
-      database: process.env.DB_NAME,
-      resourceArn: process.env.DB_ARN,
-      secretArn: process.env.DB_SECRET,
-      sql: SQL
-    };
-  
-    const command = new BatchExecuteStatementCommand(params);
-    try {
-      const data = await dbClient.send(command);
-  
-      return {'data': data};
-    } catch (error) {
-      return {'error': error};
-    }
-  //} else if (event.httpMethod == "GET") {
-    // get all forms from DB for now
-  //  return {"message": "Hello World"};
-  //}
+  if (!SQL) {
+    return {'error': "Method not supported"}
+  }
+   
+  const params = {
+    database: process.env.DB_NAME,
+    resourceArn: process.env.DB_ARN,
+    secretArn: process.env.DB_SECRET,
+    sql: SQL,
+    parameters: parameters
+  };
+
+  const command = new ExecuteStatementCommand(params);
+  try {
+    const data = await dbClient.send(command);
+
+    return {'data': data.updateResults};
+  } catch (error) {
+    return {'error': error};
+  }
 };
