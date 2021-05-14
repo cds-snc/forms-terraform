aceabfb10e5a8e5f71924f04769cce74b6c6fb3c
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-05-14 13:58:06 -0400

    ignore warning about using global encryption key

--- forms/aws/secrets.tf
+++ forms/aws/secrets.tf
@@ -6,7 +6,8 @@ resource "aws_secretsmanager_secret" "notify_api_key" {
   name                    = "notify_api_key"
   recovery_window_in_days = 0
 }
-
+# Ignore using global encryption key
+#tfsec:ignore:AWS095
 resource "aws_secretsmanager_secret_version" "notify_api_key" {
   secret_id     = aws_secretsmanager_secret.notify_api_key.id
   secret_string = var.ecs_secret_notify_api_key
