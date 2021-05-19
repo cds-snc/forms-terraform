0e22b595e63c95f05f2c59afceac34c8a091cb90
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2021-05-19 10:06:31 -0600

    build out the rest of the Lambda API methods (#23)

--- forms/aws/lambda/templates/templates.js
+++ forms/aws/lambda/templates/templates.js
@@ -7,22 +7,94 @@ exports.handler = async function (event) {
 
   let SQL,
       parameters;
+
+  /*
+    Supported Methods:
+    INSERT:
+      - (Required) json_config - json config string defining a form
+    GET:
+      - (Optional) formID. Returns all entries if not provided
+    UPDATE:
+      - (Required) formID to update
+      - (Required) json_config - new json config string to update the entry
+    DELETE:
+      - (Required) formID to delete
+  */
+
+  let formID      = (event.formID) ? event.formID : null,
+      json_config = (event.json_config) ? "'" + JSON.stringify(event.json_config) + "'" : null;
+
   switch (method) {
     case "INSERT":
-      const json_config = "'" + JSON.stringify(event.json_config) + "'";
-      SQL = "INSERT INTO Templates (json_config) VALUES (:json_config)";
-      parameters = [
-        {
-          name: "json_config",
-          typeHint: "JSON",
-          value: {
-            stringValue: JSON.stringify(event.json_config)
+      if (json_config) {
+        SQL = "INSERT INTO Templates (json_config) VALUES (:json_config)";
+        parameters = [
+          {
+            name: "json_config",
+            typeHint: "JSON",
+            value: {
+              stringValue: JSON.stringify(event.json_config)
+            }
           }
-        }
-      ];
+        ];
+      } else {
+        return {'error': "Missing required JSON"}
+      }
       break;
     case "GET":
-      SQL = "SELECT * FROM Templates";
+      // Get a specific form if given the id, all forms if not
+      if (formID) {
+        SQL = "SELECT * FROM Templates WHERE id = :formID";
+        parameters = [
+          {
+            name: "formID",
+            value: {
+              longValue: formID
+            }
+          }
+        ];
+      } else {
+        SQL = "SELECT * FROM Templates";
+      }
+      break;
+    case "UPDATE":
+      // needs the ID and the new json blob
+      if (formID && json_config) {
+        SQL = "UPDATE Templates SET json_config = :json_config WHERE id = :formID";
+        parameters = [
+          {
+            name: "formID",
+            value: {
+              longValue: formID
+            }
+          },
+          {
+            name: "json_config",
+            typeHint: "JSON",
+            value: {
+              stringValue: JSON.stringify(event.json_config)
+            }
+          }
+        ];
+      } else {
+        return {'error': "Missing required Parameter"}
+      }
+      break;
+    case "DELETE":
+      // needs the ID
+      if (formID) {
+        SQL = "DELETE from Templates WHERE id = :formID";
+        parameters = [
+          {
+            name: "formID",
+            value: {
+              longValue: formID
+            }
+          }
+        ];
+      } else {
+        return {'error': "Missing required Parameter: FormID"}
+      }
       break;
   }
 
@@ -35,6 +107,7 @@ exports.handler = async function (event) {
     resourceArn: process.env.DB_ARN,
     secretArn: process.env.DB_SECRET,
     sql: SQL,
+    includeResultMetadata: false, // set to true if we want metadata like column names
     parameters: parameters
   };
 
