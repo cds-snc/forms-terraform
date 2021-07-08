b25a5927e31f109efff18ef3b5d3aa88da6f236e
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2021-07-08 14:40:08 -0400

    Load testing lambda (#35)

--- forms/aws/lambda.tf
+++ forms/aws/lambda.tf
@@ -217,9 +217,6 @@ resource "aws_lambda_layer_version" "templates_lib" {
 ###
 # Vault Retrieval 
 ###
-###
-# AWS Lambda - Template Storage processing
-###
 
 data "archive_file" "retrieval_main" {
   type        = "zip"
@@ -260,6 +257,39 @@ resource "aws_lambda_layer_version" "retrieval_lib" {
   source_code_hash    = data.archive_file.retrieval_lib.output_base64sha256
   compatible_runtimes = ["nodejs12.x", "nodejs14.x"]
 }
+
+###
+# Load Testing
+###
+data "archive_file" "load_testing" {
+  type        = "zip"
+  source_dir  = "lambda/load_testing/"
+  output_path = "/tmp/load_testing.zip"
+}
+
+resource "aws_lambda_function" "load_testing" {
+  filename         = "/tmp/load_testing.zip"
+  function_name    = "LoadTesting"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda_locust.handler"
+  timeout          = 300
+  source_code_hash = data.archive_file.load_testing.output_base64sha256
+
+  runtime     = "python3.6"
+  description = "A function that runs a locust load test"
+
+  environment {
+    variables = {
+      LOCUST_RUN_TIME    = "3m"
+      LOCUST_LOCUSTFILE  = "locust_test_file.py"
+      LOCUST_HOST        = "https://forms-staging.cdssandbox.xyz"
+      LOCUST_HATCH_RATE  = "1"
+      LOCUST_NUM_CLIENTS = "1"
+    }
+  }
+
+}
+
 
 
 ## Allow SNS to call Lambda function
@@ -453,7 +483,7 @@ resource "aws_iam_policy" "lambda_kms" {
         "kms:Encrypt",
         "kms:Decrypt"
       ],
-      "Resource": "*",
+      "Resource": ["${aws_kms_key.dynamoDB.arn}"],
       "Effect": "Allow"
     }
   ]
