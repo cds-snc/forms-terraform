706d7a9a56ee3b7c14856d5d100a34fd7aa8a394
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-07-08 10:31:28 -0400

    load testing lambda

---
+++ forms/aws/lambda/load_testing/lambda_locust.py
@@ -0,0 +1,34 @@
+import sys
+
+sys.path.insert(0, "python-packages")
+
+import logging
+import json
+from invokust.aws_lambda import get_lambda_runtime_info
+from invokust import LocustLoadTest, create_settings
+
+logging.basicConfig(level=logging.INFO)
+
+
+def handler(event=None, context=None):
+    try:
+        if event:
+            settings = create_settings(**event)
+        else:
+            settings = create_settings(from_environment=True)
+
+        loadtest = LocustLoadTest(settings)
+        loadtest.run()
+
+    except Exception as e:
+        logging.error("Locust exception {0}".format(repr(e)))
+
+    else:
+        locust_stats = loadtest.stats()
+        lambda_runtime_info = get_lambda_runtime_info(context)
+        loadtest_results = locust_stats.copy()
+        loadtest_results.update(lambda_runtime_info)
+        json_results = json.dumps(loadtest_results)
+
+        logging.info(json_results)
+        return json_results
