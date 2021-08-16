5a1a9f6d156123d10f4a04d2665c22ffd31f636e
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2021-08-16 15:57:06 -0400

    Add s3 buckets

--- forms/aws/s3.tf
+++ forms/aws/s3.tf
@@ -23,3 +23,97 @@ resource "aws_s3_bucket_public_access_block" "firehose_waf_logs" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+
+###
+# AWS S3 bucket - Reliability Queue File Storage
+###
+resource "aws_s3_bucket" "reliability_file_storage" {
+  bucket = "forms-staging-reliability_file_storage"
+  acl    = "private"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 5
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  #tfsec:ignore:AWS002
+  #tfsec:ignore:AWS077
+
+}
+
+resource "aws_s3_bucket_public_access_block" "reliability_file_storage" {
+  bucket                  = aws_s3_bucket.reliability_file_storage.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+###
+# AWS S3 bucket - Vault File Storage
+###
+resource "aws_s3_bucket" "vault_file_storage" {
+  bucket = "forms-staging-vault_file_storage"
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  #tfsec:ignore:AWS002
+  #tfsec:ignore:AWS077
+
+}
+
+resource "aws_s3_bucket_public_access_block" "vault_file_storage" {
+  bucket                  = aws_s3_bucket.vault_file_storage.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+###
+# AWS S3 bucket - Archive Storage
+###
+resource "aws_s3_bucket" "archive_storage" {
+  bucket = "forms-staging-archive_storage"
+  acl    = "private"
+
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = 5
+    }
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  #tfsec:ignore:AWS002
+  #tfsec:ignore:AWS077
+
+}
+
+resource "aws_s3_bucket_public_access_block" "archive_storage" {
+  bucket                  = aws_s3_bucket.archive_storage.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
