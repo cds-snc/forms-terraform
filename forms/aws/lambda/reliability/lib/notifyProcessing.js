a545a91f4ed847fab8c568ed453ae9ce7c05f6e9
Clément Janin <clement.janin@cds-snc.ca>
2021-09-22 16:03:40 -0400

    Added form property to formSubmission before passing the object to either Vault or Notify processing functions

--- forms/aws/lambda/reliability/lib/notifyProcessing.js
+++ forms/aws/lambda/reliability/lib/notifyProcessing.js
@@ -13,8 +13,6 @@ const REGION = process.env.REGION;
 module.exports = async (submissionID, sendReceipt, formSubmission, message) => {
   const templateID = "92096ac6-1cc5-40ae-9052-fffdb8439a90";
   const notify = new NotifyClient("https://api.notification.canada.ca", process.env.NOTIFY_API_KEY);
-  // Add form config back to submission to be processed
-  formSubmission.form = await getFormTemplate(formSubmission.formID);
   const emailBody = convertMessage(formSubmission);
   const messageSubject = formSubmission.form.emailSubjectEn
     ? formSubmission.form.emailSubjectEn
