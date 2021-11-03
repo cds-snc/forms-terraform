e00f534aeb80f77f569bb6ab1ace04544d291df1
Pat Heard <patrick.heard@cds-snc.ca>
2021-11-03 13:59:27 -0400

    feat: Terragrunt network module (#97)
    Creates the VPC, subnets, security groups, endpoints and
    flow logs for the Forms network.
    
    Original module code in:
    e3e5be5
    5c8f647

---
+++ aws/network/network.tf
@@ -0,0 +1,171 @@
+#
+# VPC:
+# Defines the network and subnets for the Forms service
+#
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "forms" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
+
+  tags = {
+    Name                  = var.vpc_name
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+#
+# Internet Gateway:
+# Allow VPC resources to communicate with the internet
+#
+resource "aws_internet_gateway" "forms" {
+  vpc_id = aws_vpc.forms.id
+
+  tags = {
+    Name                  = var.vpc_name
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+#
+# Subnets:
+# 3 public and 3 private subnets
+#
+resource "aws_subnet" "forms_private" {
+  count = 3
+
+  vpc_id            = aws_vpc.forms.id
+  cidr_block        = cidrsubnet(var.vpc_cidr_block, 4, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name                  = "Private Subnet 0${count.index + 1}"
+    (var.billing_tag_key) = var.billing_tag_value
+    Access                = "private"
+    Terraform             = true
+  }
+}
+
+resource "aws_subnet" "forms_public" {
+  count = 3
+
+  vpc_id            = aws_vpc.forms.id
+  cidr_block        = cidrsubnet(var.vpc_cidr_block, 4, count.index + 3)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name                  = "Public Subnet 0${count.index + 1}"
+    (var.billing_tag_key) = var.billing_tag_value
+    Access                = "public"
+    Terraform             = true
+  }
+}
+
+data "aws_subnet_ids" "ecr_endpoint_available" {
+  vpc_id = aws_vpc.forms.id
+  filter {
+    name   = "tag:Access"
+    values = ["private"]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["ca-central-1a", "ca-central-1b"]
+  }
+  depends_on = [aws_subnet.forms_private]
+}
+
+data "aws_subnet_ids" "lambda_endpoint_available" {
+  vpc_id = aws_vpc.forms.id
+  filter {
+    name   = "tag:Access"
+    values = ["private"]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["ca-central-1a", "ca-central-1b"]
+  }
+  depends_on = [aws_subnet.forms_private]
+}
+
+#
+# NAT Gateway
+# Allows private resources to access the internet
+#
+resource "aws_nat_gateway" "forms" {
+  count = 3
+
+  allocation_id = aws_eip.forms_natgw.*.id[count.index]
+  subnet_id     = aws_subnet.forms_public.*.id[count.index]
+
+  tags = {
+    Name                  = "${var.vpc_name} NAT GW"
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+
+  depends_on = [aws_internet_gateway.forms]
+}
+
+resource "aws_eip" "forms_natgw" {
+  count = 3
+  vpc   = true
+
+  tags = {
+    Name                  = "${var.vpc_name} NAT GW ${count.index}"
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+#
+# Routes
+#
+resource "aws_route_table" "forms_public_subnet" {
+  vpc_id = aws_vpc.forms.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.forms.id
+  }
+
+  tags = {
+    Name                  = "Public Subnet Route Table"
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_route_table_association" "forms" {
+  count = 3
+
+  subnet_id      = aws_subnet.forms_public.*.id[count.index]
+  route_table_id = aws_route_table.forms_public_subnet.id
+}
+
+resource "aws_route_table" "forms_private_subnet" {
+  count = 3
+
+  vpc_id = aws_vpc.forms.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.forms.*.id[count.index]
+  }
+
+  tags = {
+    Name                  = "Private Subnet Route Table ${count.index}"
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_route_table_association" "forms_private_route" {
+  count = 3
+
+  subnet_id      = aws_subnet.forms_private.*.id[count.index]
+  route_table_id = aws_route_table.forms_private_subnet.*.id[count.index]
+}
