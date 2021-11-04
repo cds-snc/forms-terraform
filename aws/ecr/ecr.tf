64bac8effaefdc7691634a37f850686fc37923c3
Pat Heard <patrick.heard@cds-snc.ca>
2021-11-04 09:59:59 -0400

    feat: Terragrunt Elastic Container Registry (#104)
    Creates the ECR that holds the Form viewer Docker images
    used by the ECS cluster.

---
+++ aws/ecr/ecr.tf
@@ -0,0 +1,38 @@
+#
+# Elastic Container Registry
+# Holds the Forms app images used by the Elastic Container Service
+#
+resource "aws_ecr_repository" "viewer_repository" {
+  name                 = "form_viewer_${var.env}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "policy" {
+  repository = aws_ecr_repository.viewer_repository.name
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
