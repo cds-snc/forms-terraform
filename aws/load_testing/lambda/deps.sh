4b4def9b5f74f5789468e8a97e0a87727b198ec9
Pat Heard <patrick.heard@cds-snc.ca>
2021-11-10 13:58:35 +0000

    fix: add python-testresources
    Required by the launchpadlib dependency.

--- aws/load_testing/lambda/deps.sh
+++ aws/load_testing/lambda/deps.sh
@@ -12,5 +12,6 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 if [ "$ACTION" = "delete" ]; then
     find "$SCRIPT_DIR/load_testing" -mindepth 1 -maxdepth 1 -type d -exec rm -rvf {} +
 else
+    apt update && apt install -y python3-testresources
     pip3 install -r "$SCRIPT_DIR/load_testing/requirements.txt" --target "$SCRIPT_DIR/load_testing"
 fi
