72c164a369e33e6521b23af848670225e31427bd
Omar Nasr <omar@omarnasr.ca>
2022-01-10 14:31:09 -0500

    fix: Allow access to vault indexes by lambda
    Allow the lambda function to access the indexes on the vault table

--- aws/app/ecs_iam.tf
+++ aws/app/ecs_iam.tf
@@ -174,7 +174,8 @@ data "aws_iam_policy_document" "forms_dynamodb" {
 
     resources = [
       var.dynamodb_relability_queue_arn,
-      var.dynamodb_vault_arn
+      var.dynamodb_vault_arn,
+      "${var.dynamodb_vault_arn}/index/*"
     ]
   }
 }
