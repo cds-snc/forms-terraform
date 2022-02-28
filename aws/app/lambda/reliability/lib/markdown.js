cfb152e8d5fb0eeba23e2c93b4efdab9100c1c3e
rkeita <raphael.keita@cds-snc.ca>
2022-02-28 15:58:09 -0500

    fix: date to ISO format

--- aws/app/lambda/reliability/lib/markdown.js
+++ aws/app/lambda/reliability/lib/markdown.js
@@ -2,7 +2,6 @@ const json2md = require("json2md");
 const { extractFormData } = require("dataLayer");
 
 module.exports = (formResponse, language, submissionDateTime) => {
-
   const title = `${language === "fr" ? (formResponse.form.emailSubjectFr
       ? formResponse.form.emailSubjectFr
       : formResponse.form.titleFr): (formResponse.form.emailSubjectEn
@@ -13,11 +12,17 @@ module.exports = (formResponse, language, submissionDateTime) => {
   const mdBody = stringifiedData.map((item) => {
     return { p: item };
   });
-
+ 
+  let formatedDate;  
+  try {
+      formatedDate = submissionDateTime ? new Date(submissionDateTime * 1000).toISOString() : "";
+   }catch(error){
+      console.log(error);
+  }  
   // use Notify lang attribute to denote the language https://notification.canada.ca/format
   const emailBody = language === "fr" ?
-      "[[fr]]\n" + json2md([{ h1: title }, { h5: "Date" }, { p: submissionDateTime }, mdBody])+ "\n[[/fr]]":
-      "[[en]]\n" + json2md([{ h1: title }, { h5: "Date" }, { p: submissionDateTime }, mdBody])+ "\n[[/en]]";
+      "[[fr]]\n" + json2md([{ h1: title }, { h5: "Date" }, { p: formatedDate }, mdBody])+ "\n[[/fr]]":
+      "[[en]]\n" + json2md([{ h1: title }, { h5: "Date" }, { p: formatedDate }, mdBody])+ "\n[[/en]]";
 
   return emailBody;
 };
