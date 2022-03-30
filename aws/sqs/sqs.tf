a0b608e2f3f79d7e461158b758d6b359c6740a1c
Moro-Code <omar@omarnasr.ca>
2022-03-30 09:51:57 -0400

    fix: remove redrive allow policy

--- aws/sqs/sqs.tf
+++ aws/sqs/sqs.tf
@@ -19,11 +19,6 @@ resource "aws_sqs_queue" "reliability_queue" {
     maxReceiveCount     = 5
   })
 
-  redrive_allow_policy = jsonencode({
-    redrivePermission = "byQueue",
-    sourceQueueArns   = ["${aws_sqs_queue.deadletter_queue.arn}"]
-  })
-
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
     Terraform             = true
@@ -63,11 +58,6 @@ resource "aws_sqs_queue" "reprocess_submission_queue" {
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn
     maxReceiveCount     = 5
-  })
-
-  redrive_allow_policy = jsonencode({
-    redrivePermission = "byQueue",
-    sourceQueueArns   = ["${aws_sqs_queue.deadletter_queue.arn}"]
   })
 
   tags = {
