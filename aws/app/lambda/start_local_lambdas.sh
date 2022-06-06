be931cd06c0acdef834b4c48095fc1799a35e133
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-06 20:11:50 +0000

    fix: start dind_add_host script in `make lambdas`

--- aws/app/lambda/start_local_lambdas.sh
+++ aws/app/lambda/start_local_lambdas.sh
@@ -6,6 +6,12 @@ SCRIPT_DIR="$(dirname $(readlink -f $0))"
 # Install yarn dependencies
 find "$SCRIPT_DIR" -maxdepth 3 -name package.json -execdir yarn install \;
 
+# Start the Docker-in-Docker helper for the devcontainer
+if ! pgrep "dind_add_host" > /dev/null; then
+  echo "⚡ Start Docker-in-Docker add host"
+  nohup bash -c '/workspace/.devcontainer/scripts/dind_add_host.sh &' >/dev/null 2>&1
+fi
+
 # Start the lambda functions
 sam local start-lambda -t "$SCRIPT_DIR/local_development/template.yml" \
   --host 0.0.0.0 \
