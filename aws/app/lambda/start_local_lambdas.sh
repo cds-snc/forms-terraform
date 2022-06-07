cds-snc/forms-terraform

commit a912a8316416688b558104efd34ff97571858ff0

feat: update devcontainer to use docker-in-docker (#214)
Switch the devcontainer to use docker-in-docker so that all
AWS SAM Lambda functions are run as containers within
the devcontainer.

This allows for a simpler local dev environment setup as all
commands can be run from the same devcontainer.

aws/app/lambda/start_local_lambdas.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
-find . -maxdepth 3 -name package.json -execdir yarn install \;
+SCRIPT_DIR="$(dirname $(readlink -f $0))"
 
-# Check if running locally or in the devcontainer
-if [[ -z "${DEVCONTAINER}" ]]; then
-  sam local start-lambda -t ./local_development/template.yml --host 0.0.0.0 --port 3001 --warm-containers EAGER
-else
-  # devcontainer detected, set container host to host.docker.internal
-  sam local start-lambda -t ./local_development/template.yml --host 0.0.0.0 --port 3001 --warm-containers EAGER --container-host host.docker.internal
+# Install yarn dependencies
+find "$SCRIPT_DIR" -maxdepth 3 -name package.json -execdir yarn install \;
+
+# Start the Docker-in-Docker helper for the devcontainer
+if ! pgrep "dind_add_host" > /dev/null; then
+  echo "⚡ Start Docker-in-Docker add host"
+  nohup bash -c '/workspace/.devcontainer/scripts/dind_add_host.sh &' >/dev/null 2>&1
 fi
+
+# Start the lambda functions
+sam local start-lambda -t "$SCRIPT_DIR/local_development/template.yml" \
+  --host 0.0.0.0 \
+  --port 3001 \
+  --warm-containers EAGER

.devcontainer/Dockerfile
@@ -6,7 +6,7 @@ ARG AWS_SAM_CHECKSUM
 
 # Install packages
 RUN apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install --no-install-recommends build-essential ca-certificates curl git gnupg2 jq libffi-dev make nodejs npm openssh-client python3-dev python3-pip vim zsh \
+    && apt-get -y install --no-install-recommends build-essential ca-certificates curl dnsutils git gnupg2 jq libffi-dev make nodejs npm openssh-client python3-dev python3-pip vim zsh \
     && apt-get autoremove -y && apt-get clean -y 
 
 # Install yarn

.devcontainer/docker-compose.yml
@@ -8,7 +8,6 @@ services:
       args:
         AWS_SAM_VERSION: "1.50.0"
         AWS_SAM_CHECKSUM: "093fc2cc40b098321dcc8635abe468642b72f8658821c9243a9357e400734207"
-        
     volumes:
       - ..:/workspace:cached
     command: sleep infinity

.devcontainer/scripts/dind_add_host.sh
@@ -1,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+# Listens for Docker container `start` events and updates the new 
+# container's `/etc/hosts` file with an entry for `host.docker.internal`
+# that references the host running Docker.
+#
+# This is required because the devcontainer is running the AWS SAM lambda
+# containers using docker-in-docker (DinD), which does not add
+# this host entry.
+
+HOST_IP="$(dig +short host.docker.internal)"
+docker events \
+    --filter 'event=start' \
+    --format '{{.ID}}' | \
+    while read ID; do docker exec $ID sh -c "echo '$HOST_IP host.docker.internal' >> /etc/hosts"; done;

Makefile
@@ -22,8 +22,7 @@ terragrunt: ## Create localstack resources
 	.devcontainer/scripts/terraform_apply_localstack.sh
 
 lambdas: ## Start lambdas locally
-	cd aws/app/lambda &&\
-	./start_local_lambdas.sh
+	./aws/app/lambda/start_local_lambdas.sh
 
 local: terragrunt lambdas
 

commit d77bf08763a80bba934c5cf43667e9297c1b9d7e

fix: only run docker-in-docker helper in devcontainer (#215)
Limit the execution of the Docker-in-Docker host.docker.internal helper
script to only run when make lambdas is triggered from a devcontainer.

This fixes the use off make lambdas when run locally on the host
which was accidentally broken by #214.

aws/app/lambda/start_local_lambdas.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 SCRIPT_DIR="$(dirname $(readlink -f $0))"
 
 # Install yarn dependencies
 find "$SCRIPT_DIR" -maxdepth 3 -name package.json -execdir yarn install \;
 
-# Start the Docker-in-Docker helper for the devcontainer
-if ! pgrep "dind_add_host" > /dev/null; then
-  echo "⚡ Start Docker-in-Docker add host"
-  nohup bash -c '/workspace/.devcontainer/scripts/dind_add_host.sh &' >/dev/null 2>&1
+# Devcontainer specific startup commands
+if [ -n "${DEVCONTAINER}" ]; then
+
+  # Start the Docker-in-Docker helper in the bacgkround if it's not already running
+  if ! pgrep "dind_add_host" > /dev/null; then
+    echo "⚡ Starting Docker-in-Docker add host helper"
+    nohup bash -c '/workspace/.devcontainer/scripts/dind_add_host.sh &' >/dev/null 2>&1
+  fi
+
 fi
 
 # Start the lambda functions