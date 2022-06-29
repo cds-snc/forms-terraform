6ded9df5be68c8dd4f811e613212af07492a6ffe
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-29 17:57:00 +0000

    feat: add S3 object scan to Vault S3 bucket
    This will enable Scan Files `ClamAV` scans on all new objects
    created in the Vault S3 bucket.  The scan status and verdict
    will be recorded via S3 object tags.

---
+++ aws/app/vault_scan_object.tf
@@ -0,0 +1,9 @@
+module "vault_scan_object" {
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.3//S3_scan_object"
+
+  product_name            = "vault"
+  s3_upload_bucket_name   = aws_s3_bucket.vault_file_storage.id
+  s3_upload_bucket_create = false
+
+  billing_tag_value = var.billing_tag_value
+}
