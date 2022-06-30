9e8776672ac163acc18f6ecb66eb90c53e57e9aa
Pat Heard <patrick.heard@cds-snc.ca>
2022-06-30 15:11:29 +0000

    chore: bump to latest module version

--- aws/app/vault_scan_object.tf
+++ aws/app/vault_scan_object.tf
@@ -1,5 +1,5 @@
 module "vault_scan_object" {
-  source = "github.com/cds-snc/terraform-modules?ref=v3.0.3//S3_scan_object"
+  source = "github.com/cds-snc/terraform-modules?ref=v3.0.5//S3_scan_object"
 
   product_name            = "vault"
   s3_upload_bucket_name   = aws_s3_bucket.vault_file_storage.id
