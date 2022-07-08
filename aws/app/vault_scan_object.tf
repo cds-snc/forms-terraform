87202e14bf9255179c8099cc068a2e583c504060
Pat Heard <patrick.heard@cds-snc.ca>
2022-07-08 01:22:18 +0000

    fix: remove outdated module input

--- aws/app/vault_scan_object.tf
+++ aws/app/vault_scan_object.tf
@@ -1,9 +1,8 @@
 module "vault_scan_object" {
   source = "github.com/cds-snc/terraform-modules?ref=v3.0.9//S3_scan_object"
 
-  product_name            = "vault"
-  s3_upload_bucket_name   = aws_s3_bucket.vault_file_storage.id
-  s3_upload_bucket_create = false
+  product_name          = "vault"
+  s3_upload_bucket_name = aws_s3_bucket.vault_file_storage.id
 
   billing_tag_value = var.billing_tag_value
 }
