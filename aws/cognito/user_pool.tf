485112f855352febd371c09f04c5daaa77d0cef4
Moro-Code <omar@omarnasr.ca>
2022-09-21 15:51:06 -0400

    feat: Setup cognito for server side auth
    * enable user self signup
    * enable verification of email
    * set email attribute as email

--- aws/cognito/user_pool.tf
+++ aws/cognito/user_pool.tf
@@ -1,5 +1,7 @@
 resource "aws_cognito_user_pool" "forms" {
-  name = "forms_user_pool"
+  name                     = "forms_user_pool"
+  auto_verified_attributes = ["email"]
+  username_attributes      = ["email"]
   password_policy {
     minimum_length    = 8
     require_lowercase = true
@@ -13,14 +15,17 @@ resource "aws_cognito_user_pool" "forms" {
       priority = 1
     }
   }
-  username_configuration {
-    case_sensitive = true
-  }
   email_configuration {
     email_sending_account = "COGNITO_DEFAULT"
   }
+  verification_message_template {
+    default_email_option  = "CONFIRM_WITH_LINK"
+    email_message_by_link = "Please confirm your email / S.V.P confirmer votre email {##Click Here##}"
+    email_subject_by_link = "Please confirm your email for GC Forms / S.V.P confirmer votre email pour GC Formulaire  "
+  }
+
   admin_create_user_config {
-    allow_admin_create_user_only = true
+    allow_admin_create_user_only = false
     invite_message_template {
       email_message = "Welcome!  Please log in using the following credentials:/n  Username: {username}/n  Password: {####}"
       email_subject = "Welcome to GCForms"
@@ -39,6 +44,7 @@ resource "aws_cognito_user_pool_client" "forms" {
   allowed_oauth_flows                  = ["code"]
   allowed_oauth_scopes                 = ["email", "openid", "profile"]
   supported_identity_providers         = ["COGNITO"]
+  explicit_auth_flows                  = ["ALLOW_ADMIN_USER_PASSWORD_AUTH"]
   generate_secret                      = true
 }
 
