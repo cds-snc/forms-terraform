f6c623cfad11d6dbd70a1973898b6661409f1fec
Moro-Code <omar@omarnasr.ca>
2022-09-22 14:59:31 -0400

    fix: Remove client secret from app client
    * remove client secret from app client
    * set email verification to be with code

--- aws/cognito/user_pool.tf
+++ aws/cognito/user_pool.tf
@@ -19,9 +19,9 @@ resource "aws_cognito_user_pool" "forms" {
     email_sending_account = "COGNITO_DEFAULT"
   }
   verification_message_template {
-    default_email_option  = "CONFIRM_WITH_CODE"
-    email_message = "Please confirm your email / S.V.P confirmer votre email {####}"
-    email_subject = "Please confirm your email for GC Forms / S.V.P confirmer votre email pour GC Formulaire  "
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_message        = "Please confirm your email / S.V.P confirmer votre email {####}"
+    email_subject        = "Please confirm your email for GC Forms / S.V.P confirmer votre email pour GC Formulaire  "
   }
 
   admin_create_user_config {
