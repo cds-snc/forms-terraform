80cc30a0c8183f1d4e359f9904dfb0166bcb4449
Moro-Code <omar@omarnasr.ca>
2022-09-22 15:25:43 -0400

    remove reference to client secret from cognito module outputs

--- aws/cognito/outputs.tf
+++ aws/cognito/outputs.tf
@@ -7,8 +7,3 @@ output "cognito_client_id" {
   description = "Client ID of the forms user pool client."
   value       = aws_cognito_user_pool_client.forms.id
 }
-
-output "cognito_client_secret_arn" {
-  description = "Forms client cognito secret arn"
-  value       = aws_secretsmanager_secret_version.cognito_client_secret.arn
-}
