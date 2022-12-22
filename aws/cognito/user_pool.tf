092f7ed51a26ad9e560136e8e32f5cc5908348e3
Omar Nasr <omar@omarnasr.ca>
2022-12-22 11:22:58 -0500

    fix: only use supported lambda_version value in userpool (#301)

--- aws/cognito/user_pool.tf
+++ aws/cognito/user_pool.tf
@@ -43,3 +43,10 @@ resource "aws_cognito_user_pool_domain" "forms" {
   domain       = "forms"
   user_pool_id = aws_cognito_user_pool.forms.id
 }
+
+resource "aws_lambda_permission" "allow_cognito" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cognito_email_sender.function_name
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.forms.arn
+}
