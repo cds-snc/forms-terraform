e77aa4fcde61acb9b0397b698d310c9cc984ae0e
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2022-12-28 15:34:37 -0500

    Associate policy to role (#305)
    * Add role for cognito userpool import logging
    
    * associate policy to role

--- aws/cognito/user_pool_iam.tf
+++ aws/cognito/user_pool_iam.tf
@@ -32,6 +32,11 @@ resource "aws_iam_policy" "cognito_userpool_import_logging" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "cognito_userpool_import_logs" {
+  role       = aws_iam_role.cognito_userpool_import.name
+  policy_arn = aws_iam_policy.cognito_userpool_import_logging.arn
+}
+
 
 data "aws_iam_policy_document" "cognito_userpool_import_logging" {
   statement {
