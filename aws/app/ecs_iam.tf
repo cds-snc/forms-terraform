43adf3693f5393b3f571fc6d975d1feca68674ac
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-03-09 15:14:39 -0500

    Remove extra comma

--- aws/app/ecs_iam.tf
+++ aws/app/ecs_iam.tf
@@ -230,7 +230,7 @@ data "aws_iam_policy_document" "forms_dynamodb" {
     resources = [
       var.dynamodb_relability_queue_arn,
       var.dynamodb_vault_arn,
-      "${var.dynamodb_vault_arn}/index/*",
+      "${var.dynamodb_vault_arn}/index/*"
     ]
   }
 }
