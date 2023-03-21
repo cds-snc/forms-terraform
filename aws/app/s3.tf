3eecca7272e22db046a1bc88febcf49c7f8c59e6
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-03-21 13:57:02 -0400

    S3 bucket upgrades

--- aws/app/s3.tf
+++ aws/app/s3.tf
@@ -5,26 +5,36 @@ resource "aws_s3_bucket" "reliability_file_storage" {
   # checkov:skip=CKV_AWS_18: Versioning not required
   # checkov:skip=CKV_AWS_21: Access logging not required  
   bucket = "forms-${var.env}-reliability-file-storage"
-  acl    = "private"
-
-  lifecycle_rule {
-    enabled = true
-
-    expiration {
-      days = 30
-    }
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
     Terraform             = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "reliability_file_storage" {
+  bucket = aws_s3_bucket.reliability_file_storage.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "reliability_file_storage" {
+  bucket = aws_s3_bucket.reliability_file_storage.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "reliability_file_storage" {
+  bucket = aws_s3_bucket.reliability_file_storage.id
+
+  rule {
+    id     = "Keep files for 30 days"
+    status = "Enabled"
+    expiration {
+      days = 30
+    }
   }
 }
 
@@ -40,26 +50,33 @@ resource "aws_s3_bucket_public_access_block" "reliability_file_storage" {
 # Vault File Storage
 #
 resource "aws_s3_bucket" "vault_file_storage" {
-  # checkov:skip=CKV_AWS_18: Versioning not required
   # checkov:skip=CKV_AWS_21: Access logging not required  
   bucket = "forms-${var.env}-vault-file-storage"
-  acl    = "private"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  versioning {
-    enabled = true
-  }
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
     Terraform             = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "vault_file_storage" {
+  bucket = aws_s3_bucket.vault_file_storage.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_acl" "vault_file_storage" {
+  bucket = aws_s3_bucket.vault_file_storage.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "reliability_file_storage" {
+  bucket = aws_s3_bucket.reliability_file_storage.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
@@ -78,26 +95,36 @@ resource "aws_s3_bucket" "archive_storage" {
   # checkov:skip=CKV_AWS_18: Versioning not required
   # checkov:skip=CKV_AWS_21: Access logging not required
   bucket = "forms-${var.env}-archive-storage"
-  acl    = "private"
-
-  lifecycle_rule {
-    enabled = true
-    expiration {
-      days = 30
-    }
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
     Terraform             = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "archive_storage" {
+  bucket = aws_s3_bucket.archive_storage.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "archive_storage" {
+  bucket = aws_s3_bucket.archive_storage.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "archive_storage" {
+  bucket = aws_s3_bucket.archive_storage.id
+
+  rule {
+    id     = "Keep files for 30 days"
+    status = "Enabled"
+    expiration {
+      days = 30
+    }
   }
 }
 
