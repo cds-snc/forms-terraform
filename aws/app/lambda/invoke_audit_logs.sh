7e97a9163751bcd0c782fed0d9f360a5ee89f9e4
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-03-21 12:05:50 -0400

    fix pascal vs camelCase for CLI / Lambda

--- aws/app/lambda/invoke_audit_logs.sh
+++ aws/app/lambda/invoke_audit_logs.sh
@@ -10,7 +10,16 @@ export AWS_REGION=ca-central-1
 
 # Get Queue URL
 QUEUE_URL=$(aws sqs get-queue-url --queue-name audit_log_queue --endpoint-url http://localhost:4566 --query QueueUrl --output text)
-PYTHON_CODE=$'
+PYTHON_CODE_LAMBDA_INPUT=$'
+import sys, json
+raw_data=sys.stdin.read()
+data=json.loads(raw_data)
+new_data=[]
+for x in data: new_data.append({\'messageId\':x[\'MessageId\'], \'body\':x[\'Body\']})
+print(json.dumps(new_data))
+'
+
+PYTHON_CODE_RECEIPT_HANDLES=$'
 import sys, json
 raw_data=sys.stdin.read()
 data=json.loads(raw_data)
@@ -22,15 +31,17 @@ while :; do
   echo "Raw SQS Messages: $RAW_MESSAGE"
 
   if [[ $RAW_MESSAGE == "null" ]]; then
-    echo "No messages to process"
+    echo "No messages to process"s
     break
   fi
 
   RECEIPT_HANDLES=()
-  while IFS='' read -r line; do RECEIPT_HANDLES+=("$line"); done < <(echo "$RAW_MESSAGE" | python3 -c "$PYTHON_CODE")
+  while IFS='' read -r line; do RECEIPT_HANDLES+=("$line"); done < <(echo "$RAW_MESSAGE" | python3 -c "$PYTHON_CODE_RECEIPT_HANDLES")
 
-  echo "Running Reliability Queue Lambda"
-  echo '{"Records":'"$RAW_MESSAGE"'}' | sam local invoke -t ./local_development/template.yml --event - "AuditLogs"
+  LAMBDA_INPUT=$(echo "$RAW_MESSAGE" | python3 -c "$PYTHON_CODE_LAMBDA_INPUT")
+
+  echo "Running Audit Log Lambda"
+  echo '{"Records":'"$LAMBDA_INPUT"'}' | sam local invoke -t ./local_development/template.yml --event - "AuditLogs"
 
   echo "Deleting message from SQS Queue"
 
