6565032e31c3b33b84f8862236a65ce412897dfe
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-05-04 10:37:19 -0400

    Use right length cmd

--- aws/ecr/outputs.tf
+++ aws/ecr/outputs.tf
@@ -5,5 +5,5 @@ output "ecr_repository_url_form_viewer" {
 
 output "ecr_repository_url_load_test" {
   description = "URL of the Form viewer ECR"
-  value       = aws_ecr_repository.load_test_repository.length > 0 ? aws_ecr_repository.load_test_repository[0].repository_url : ""
+  value       = length(aws_ecr_repository.load_test_repository) > 0 ? aws_ecr_repository.load_test_repository[0].repository_url : ""
 }
