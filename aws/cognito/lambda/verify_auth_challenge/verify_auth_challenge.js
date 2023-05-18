e2f96f8b9c64fd3f73779d01fd9a968557d9dfcb
Clément JANIN <clement.janin@cds-snc.ca>
2023-05-18 10:17:06 -0400

    feat: removed Cognito 2FA (#392)

--- aws/cognito/lambda/verify_auth_challenge/verify_auth_challenge.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event) => {
-  const expectedAnswer = event.request.privateChallengeParameters["verificationCode"];
-  if (event.request.challengeAnswer === expectedAnswer) {
-    event.response.answerCorrect = true;
-  } else {
-    event.response.answerCorrect = false;
-  }
-  return event;
-};
