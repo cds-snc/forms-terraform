7df17c552db98d2553980601f87197d7c2b57825
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-06-13 11:38:04 -0400

    Add conditional

--- aws/pr_review/iam.tf
+++ aws/pr_review/iam.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role" "forms_lambda_client" {
   count              = var.env == "staging" ? 1 : 0
   name               = "forms-lambda-client"
-  assume_role_policy = data.aws_iam_policy_document.forms_lambda_client.json
+  assume_role_policy = data.aws_iam_policy_document.forms_lambda_client[0].json
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
@@ -10,6 +10,7 @@ resource "aws_iam_role" "forms_lambda_client" {
 }
 
 data "aws_iam_policy_document" "forms_lambda_client" {
+  count = var.env == "staging" ? 1 : 0
   statement {
     actions = ["sts:AssumeRole"]
     effect  = "Allow"
@@ -22,6 +23,7 @@ data "aws_iam_policy_document" "forms_lambda_client" {
 }
 
 data "aws_iam_policy_document" "forms_lambda_parameter_store" {
+  count = var.env == "staging" ? 1 : 0
   statement {
     actions = ["ssm:GetParameters"]
     effect  = "Allow"
@@ -32,9 +34,10 @@ data "aws_iam_policy_document" "forms_lambda_parameter_store" {
 }
 
 resource "aws_iam_policy" "forms_lambda_parameter_store" {
+  count  = var.env == "staging" ? 1 : 0
   name   = "formsLambdaParameterStoreRetrieval"
   path   = "/"
-  policy = data.aws_iam_policy_document.forms_lambda_parameter_store.json
+  policy = data.aws_iam_policy_document.forms_lambda_parameter_store[0].json
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
@@ -45,7 +48,7 @@ resource "aws_iam_policy" "forms_lambda_parameter_store" {
 resource "aws_iam_role_policy_attachment" "forms_lambda_parameter_store" {
   count      = var.env == "staging" ? 1 : 0
   role       = aws_iam_role.forms_lambda_client[0].name
-  policy_arn = aws_iam_policy.forms_lambda_parameter_store.arn
+  policy_arn = aws_iam_policy.forms_lambda_parameter_store[0].arn
 }
 resource "aws_iam_role_policy_attachment" "secrets_manager_forms_lambda_client" {
   count      = var.env == "staging" ? 1 : 0
