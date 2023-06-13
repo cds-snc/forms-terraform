9957954325c1f066eb39cad6c92600c6474fbaf0
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2023-06-13 11:30:12 -0400

    Add iam policy to grant parameter store access to lambda client role

--- aws/pr_review/iam.tf
+++ aws/pr_review/iam.tf
@@ -21,6 +21,32 @@ data "aws_iam_policy_document" "forms_lambda_client" {
   }
 }
 
+data "aws_iam_policy_document" "forms_lambda_parameter_store" {
+  statement {
+    actions = ["ssm:GetParameters"]
+    effect = "Allow"
+    resources = [
+      "arn:aws:ssm:ca-central-1:687401027353:parameter/form-viewer/env"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "forms_lambda_parameter_store" {
+  name   = "formsLambdaParameterStoreRetrieval"
+  path   = "/"
+  policy = data.aws_iam_policy_document.forms_lambda_parameter_store.json
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "forms_lambda_parameter_store" {
+  count      = var.env == "staging" ? 1 : 0
+  role       = aws_iam_role.forms_lambda_client[0].name
+  policy_arn = aws_iam_policy.forms_lambda_parameter_store.arn
+}
 resource "aws_iam_role_policy_attachment" "secrets_manager_forms_lambda_client" {
   count      = var.env == "staging" ? 1 : 0
   role       = aws_iam_role.forms_lambda_client[0].name
