52b57c5bfbba79e8e48c8d919066b4b7415189d4
Pat Heard <patrick.heard@cds-snc.ca>
2023-06-14 14:17:52 +0000

    fix: role reference

--- aws/pr_review/lambda.tf
+++ aws/pr_review/lambda.tf
@@ -2,6 +2,6 @@ resource "aws_lambda_permission" "submission_lambda_invoke" {
   count         = var.env == "staging" ? 1 : 0
   statement_id  = "AllowSubmissionInvokeFromPRReviewEnvs"
   action        = "lambda:InvokeFunction"
-  principal     = aws_iam_role.forms_lambda_client.arn
+  principal     = aws_iam_role.forms_lambda_client[0].arn
   function_name = var.forms_submission_lambda_name
 }
