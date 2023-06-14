be97bfc7202c38a8eeae13e8452271d44a92595d
Pat Heard <patrick.heard@cds-snc.ca>
2023-06-14 14:12:04 +0000

    fix: Submission lambda permission for PR review env
    Update the Submission lambda function permissions to grant
    the PR review env's IAM role the ability to invoke the function.

--- aws/pr_review/lambda.tf
+++ aws/pr_review/lambda.tf
@@ -1,8 +1,7 @@
 resource "aws_lambda_permission" "submission_lambda_invoke" {
-  count          = var.env == "staging" ? 1 : 0
-  statement_id   = "AllowSubmissionInvokeFromPRReviewEnvs"
-  action         = "lambda:InvokeFunction"
-  principal      = "lambda.amazonaws.com"
-  function_name  = var.forms_submission_lambda_name
-  source_account = var.account_id
+  count         = var.env == "staging" ? 1 : 0
+  statement_id  = "AllowSubmissionInvokeFromPRReviewEnvs"
+  action        = "lambda:InvokeFunction"
+  principal     = aws_iam_role.forms_lambda_client.arn
+  function_name = var.forms_submission_lambda_name
 }
