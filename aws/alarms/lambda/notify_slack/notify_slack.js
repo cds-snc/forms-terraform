835e40b361e8613904ce093e283853542a6e031b
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-06-27 15:11:49 -0400

    update lambda for new streams

--- aws/alarms/lambda/notify_slack/notify_slack.js
+++ aws/alarms/lambda/notify_slack/notify_slack.js
@@ -1,5 +1,14 @@
 var https = require("https");
 var util = require("util");
+var zlib = require("zlib");
+
+function safeJsonParse(str) {
+  try {
+    return JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+}
 
 function getMessage(message) {
   try {
@@ -9,59 +18,69 @@ function getMessage(message) {
     return message;
   }
 }
-exports.handler = function (event, context) {
+
+function getSNSMessageSeverity(message) {
+  const errorMessages = ["Error", "Critical"];
+  const warningMessages = ["Warning", "FAILURE"];
+  var severity = "info";
+  var keepProcessing = true;
+
+  for (var errorMessagesItem in warningMessages) {
+    if (
+      message.indexOf(errorMessages[errorMessagesItem]) != -1 &&
+      // Does not inform about OK status
+      message.indexOf('"NewStateValue":"OK"') == -1
+    ) {
+      severity = "error";
+      keepProcessing = false;
+    } else if (message.indexOf('"NewStateValue":"OK"') != -1) {
+      severity = "alarm_reset";
+      keepProcessing = false;
+    }
+  }
+
+  // Don't bother processing if we've already found an error
+  if (!keepProcessing) {
+    return severity;
+  }
+
+  for (var warningMessagesItem in warningMessages) {
+    if (
+      message.indexOf(warningMessages[warningMessagesItem]) != -1 &&
+      // Does not inform about OK status
+      message.indexOf('"NewStateValue":"OK"') == -1
+    ) {
+      severity = "warning";
+    } else if (message.indexOf('"NewStateValue":"OK"') != -1) {
+      severity = "alarm_reset";
+    }
+  }
+
+  return severity;
+}
+
+function sendToSlack(message, severity, context) {
   var environment = process.env.ENVIRONMENT || "Staging";
+
+  const icon_emoji = (severity) => {
+    switch (severity) {
+      case "danger":
+      case "error":
+        return ":rotating_light:";
+      case "warning":
+      case "warn":
+        return ":warning:";
+      default:
+        return ":loudspeaker:";
+    }
+  };
+
   var postData = {
     channel: "#forms-deploy-activities",
     username: "Forms Notifier",
     text: `*${environment} Environment*`,
-    icon_emoji: ":loudspeaker:",
+    icon_emoji: icon_emoji(severity),
   };
-
-  var message = event.Records[0].Sns.Message;
-
-  var severity = "good";
-  var needs_processing = true;
-
-  var dangerMessages = ["End User Forms Critical"];
-
-  var warningMessages = ["End User Forms Warning", "FAILURE for deployment"];
-
-  var okMessages = ['"NewStateValue":"OK"'];
-
-  for (var okMessagesItem in okMessages) {
-    if (message.indexOf(okMessages[okMessagesItem]) != -1) {
-      message = "Alarm Status now OK - " + getMessage(message);
-      needs_processing = false;
-      break;
-    }
-  }
-
-  // Only check for critial messages if necessary
-  if (needs_processing) {
-    for (var dangerMessagesItem in dangerMessages) {
-      if (message.indexOf(dangerMessages[dangerMessagesItem]) != -1) {
-        severity = "danger";
-        postData.icon_emoji = ":rotating_light:";
-        message = getMessage(message);
-        needs_processing = false;
-        break;
-      }
-    }
-  }
-
-  // Only check for warning messages if necessary
-  if (needs_processing) {
-    for (var warningMessagesItem in warningMessages) {
-      if (message.indexOf(warningMessages[warningMessagesItem]) != -1) {
-        severity = "warning";
-        postData.icon_emoji = ":warning:";
-        message = getMessage(message);
-        needs_processing = false;
-        break;
-      }
-    }
-  }
 
   postData.attachments = [
     {
@@ -79,15 +98,52 @@ exports.handler = function (event, context) {
 
   var req = https.request(options, function (res) {
     res.setEncoding("utf8");
-    res.on("data", function (chunk) {
-      context.done(null);
+    res.on("data", function () {
+      context.succeed(
+        `Message successfully sent to Slack... severity: ${severity}, message: ${message}`
+      );
     });
   });
 
   req.on("error", function (e) {
     console.log("problem with request: " + e.message);
+    context.fail(e);
   });
 
   req.write(util.format("%j", postData));
   req.end();
+}
+
+exports.handler = function (input, context) {
+  if (input.awslogs) {
+    // This is a CloudWatch log event
+    var payload = Buffer.from(input.awslogs.data, "base64");
+    zlib.gunzip(payload, function (e, result) {
+      if (e) {
+        context.fail(e);
+      } else {
+        result = JSON.parse(result.toString());
+
+        const logMessage = safeJsonParse(result.message);
+        if (logMessage) {
+          sendToSlack(logMessage.msg, logMessage.level, context);
+          console.log("Event Data:", JSON.stringify(logMessage, null, 2));
+        } else {
+          // These are unhandled errors in the GCForms app
+          sendToSlack(result.message, "error", context);
+          console.log("Event Data:", JSON.stringify(result.message, null, 2));
+        }
+      }
+    });
+  } else {
+    // This is an SNS message triggered by an AWS CloudWatch alarm
+    var message = input.Records[0].Sns.Message;
+
+    const severity = getSNSMessageSeverity(message);
+
+    if (severity === "alarm_reset") {
+      message = "Alarm Status now OK - " + getMessage(message);
+    }
+    sendToSlack(message, severity, context);
+  }
 };
