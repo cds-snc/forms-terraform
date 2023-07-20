48377aa46fc2cb7786dc724b183356d147c861b9
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2023-07-20 08:35:38 -0400

    fix invalid syntax (#441)

--- aws/load_balancer/waf.tf
+++ aws/load_balancer/waf.tf
@@ -282,11 +282,11 @@ resource "aws_wafv2_regex_pattern_set" "valid_app_uri_paths" {
   description = "Regex to match the app valid urls"
 
   regular_expression {
-    regex_string = "^\\/(?:en|fr)?\\/?(?:(admin|id|api|auth|signup|myforms|unsupported-browser|terms-of-use|404)(?:\\/[\\w-]+)?+)(?:\\/.*)?$"
+    regex_string = "^\\/(?:en|fr)?\\/?(?:(admin|id|api|auth|signup|myforms|unsupported-browser|terms-of-use|404)(?:\\/[\\w-]+)?)(?:\\/.*)?$"
   }
 
   regular_expression {
-    regex_string = "^\\/(?:en|fr)?\\/?(?:(form-builder|sla|unlock-publishing|terms-and-conditions|javascript-disabled)(?:\\/[\\w-]+)?+)(?:\\/.*)?$"
+    regex_string = "^\\/(?:en|fr)?\\/?(?:(form-builder|sla|unlock-publishing|terms-and-conditions|javascript-disabled)(?:\\/[\\w-]+)?)(?:\\/.*)?$"
   }
 
   regular_expression {
