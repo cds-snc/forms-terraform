eaf9fb52d58a2dcaa560f73c4e2d33cbd767fc35
Daine Trinidad <daine.trinidad@cds-snc.ca>
2023-07-24 09:55:32 -0700

    chore: re-adding template file that got lost during merge
    Signed-off-by: Daine Trinidad <daine.trinidad@cds-snc.ca>

---
+++ aws/app/lambda/nagware/lib/templates.js
@@ -0,0 +1,126 @@
+const { RDSDataClient, ExecuteStatementCommand } = require("@aws-sdk/client-rds-data");
+const { Client } = require("pg");
+
+// rename to templates.js?
+
+// getFormPublishStatus
+
+async function getTemplateInfo(formID) {
+  try {
+    const { SQL, parameters } = createSQLString(formID);
+    const requestFornNameAndOwnerEmailAddress = process.env.AWS_SAM_LOCAL ? requestSAM : requestRDS;
+    const formNameAndOwnerEmailAddress = await requestFornNameAndOwnerEmailAddress(SQL, parameters);
+
+    if (formNameAndOwnerEmailAddress) {
+      return formNameAndOwnerEmailAddress;
+    } else {
+      throw new Error(`Could not find any owner email address associated to Form ID: ${formID}.`);
+    }
+  } catch (error) {
+    throw new Error(`Failed to retrieve owner email address. Reason: ${error.message}.`);
+  }
+}
+
+const createSQLString = (formID) => {
+  // TODO: originally thought about putting conditional check here
+  // BUT -- just get the tem."isPublished" value and return it
+  const selectSQL = `SELECT usr."email", tem."name", tem."jsonConfig", tem."isPublished" FROM "User" usr JOIN "_TemplateToUser" ttu ON usr."id" = ttu."B" JOIN "Template" tem ON tem."id" = ttu."A"`;
+  if (!process.env.AWS_SAM_LOCAL) {
+    return {
+      SQL: `${selectSQL} WHERE ttu."A" = :formID`,
+      parameters: [
+        {
+          name: "formID",
+          value: {
+            stringValue: formID,
+          },
+        },
+      ],
+    };
+  } else {
+    return {
+      SQL: `${selectSQL} WHERE ttu."A" = $1`,
+      parameters: [formID],
+    };
+  }
+};
+
+const parseQueryResponse = (records) => {
+  if (records.length === 1) {
+    const record = records[0];
+    if (!process.env.AWS_SAM_LOCAL) {
+      const jsonConfig = JSON.parse(record[2].stringValue.trim(1, -1)) || undefined;
+      const name = record[1].stringValue !== "" ? record[1].stringValue : `${jsonConfig.titleEn} - ${jsonConfig.titleFr}`;
+      // TODO: Not sure what field the isPublished value is in, but based on the SQL query, it should be in the 4th index
+      return { name, emailAddress: record[0].stringValue, isPublished: record[3].booleanValue };
+    } else {
+      const name = record.name !== "" ? record.name : `${record.jsonConfig.titleEn} - ${record.jsonConfig.titleFr}`;
+      return { name, emailAddress: record.email, isPublished: record.isPublished };
+    }
+  } else {
+    return null;
+  }
+};
+
+/**
+ * Creates and processes request to LOCAL AWS SAM DB
+ * @param {string} SQL
+ * @param {string[]} parameters
+ * @returns PG Client return value
+ */
+const requestSAM = async (SQL, parameters) => {
+  // Placed outside of try block to be referenced in finally
+  const dbClient = new Client();
+  try {
+    if (
+      process.env.PGHOST &&
+      process.env.PGUSER &&
+      process.env.PGDATABASE &&
+      process.env.PGPASSWORD
+    ) {
+      dbClient.connect();
+    } else {
+      throw new Error("Missing Environment Variables for DB config");
+    }
+    const data = await dbClient.query(SQL, parameters);
+    return parseQueryResponse(data.rows);
+  } catch (error) {
+    console.error(`{"status": "error", "error": "${error.message}"}`);
+    // Lift more generic error to be able to capture event info higher in scope
+    throw new Error("Error connecting to LOCAL AWS SAM DB");
+  } finally {
+    dbClient.end();
+  }
+};
+
+/**
+ * Creates and processes request to RDS
+ * @param {string} SQL
+ * @param {{name: string, value: {stringValue: string}[]}} parameters
+ * @returns RDS client return value
+ */
+const requestRDS = async (SQL, parameters) => {
+  try {
+    const dbClient = new RDSDataClient({ region: process.env.REGION });
+    const params = {
+      database: process.env.DB_NAME,
+      resourceArn: process.env.DB_ARN,
+      secretArn: process.env.DB_SECRET,
+      sql: SQL,
+      includeResultMetadata: false, // set to true if we want metadata like column names
+      parameters: parameters,
+    };
+    const command = new ExecuteStatementCommand(params);
+
+    const data = await dbClient.send(command);
+    return parseQueryResponse(data.records);
+  } catch (error) {
+    console.error(`{"status": "error", "error": "${error.message}"}`);
+    // Lift more generic error to be able to capture event info higher in scope
+    throw new Error("Error connecting to RDS");
+  }
+};
+
+module.exports = {
+  getTemplateInfo,
+};
