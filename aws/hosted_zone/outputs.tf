32d2f9de50bd3b141020f0ba230323f7cec8d183
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-10-19 14:17:17 -0400

    try list to string

--- aws/hosted_zone/outputs.tf
+++ aws/hosted_zone/outputs.tf
@@ -1,9 +1,9 @@
 output "hosted_zone_id" {
   description = "Route53 hosted zone ID"
-  value       = "[${aws_route53_zone.form_viewer.*.zone_id}]"
+  value       = "[${join("\", \"", aws_route53_zone.form_viewer.*.zone_id)}]"
 }
 
 output "hosted_zone_name" {
   description = "Route53 hosted zone name"
-  value       = "[${aws_route53_zone.form_viewer.*.name}]"
+  value       = "[${join("\", \"", aws_route53_zone.form_viewer.*.name)}]"
 }
