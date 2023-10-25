c666fba0384c2ef505da4d9b64a8b272b5473d5d
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-10-25 08:34:15 -0400

    formatting

--- aws/load_balancer/certificates.tf
+++ aws/load_balancer/certificates.tf
@@ -3,8 +3,8 @@
 #
 resource "aws_acm_certificate" "form_viewer" {
   # First entry in domain list is the primary domain
-  domain_name               = var.domains[0]
-  validation_method         = "DNS"
+  domain_name       = var.domains[0]
+  validation_method = "DNS"
   # subject_alternative_names = length(var.domains) > 1 ? setsubtract(var.domains, [var.domains[0]]) : []
 
   lifecycle {
