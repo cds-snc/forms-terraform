bd9045e483c865fbb3080ff8c6ada4678546db27
Clément Janin <clement.janin@cds-snc.ca>
2023-11-09 11:18:56 -0500

    feat: redirect to static maintenance web page when in maintenance mode or service is down

---
+++ aws/load_balancer/cloudfront.tf
@@ -0,0 +1,65 @@
+locals {
+  s3_origin_id = "MaintenanceMode"
+}
+
+resource "aws_cloudfront_origin_access_identity" "maintenance_mode" {
+  comment = "Access Identity for the Maintenance Website"
+}
+
+resource "aws_cloudfront_distribution" "maintenance_mode" {
+  # checkov:skip=CKV_AWS_68: WAF ACL not required
+  # checkov:skip=CKV_AWS_86: Access logging not required
+  enabled             = true
+  http_version        = "http2"
+  default_root_object = "index.html"
+  web_acl_id          = aws_wafv2_web_acl.forms_acl.arn
+  aliases             = var.domains
+
+  origin {
+    origin_id   = local.s3_origin_id
+    domain_name = aws_s3_bucket.maintenance_mode.bucket_regional_domain_name
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.maintenance_mode.cloudfront_access_identity_path
+    }
+  }
+
+  default_cache_behavior {
+    compress         = true
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+
+  depends_on = [
+    aws_s3_bucket.maintenance_mode
+  ]
+}
---
+++ aws/load_balancer/s3.tf
@@ -0,0 +1,77 @@
+resource "aws_s3_bucket" "maintenance_mode" {
+  # checkov:skip=CKV_AWS_18: Versioning not required
+  # checkov:skip=CKV_AWS_19: False-positive, server side encryption is enabled but probably not detected because defined in a different Terraform resource
+  # checkov:skip=CKV_AWS_21: Access logging not required
+  bucket = "gc-forms-application-maintenance-page"
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+    Terraform             = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "maintenance_mode" {
+  bucket = aws_s3_bucket.maintenance_mode.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "maintenance_mode" {
+  bucket = aws_s3_bucket.maintenance_mode.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "maintenance_mode" {
+  bucket                  = aws_s3_bucket.maintenance_mode.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_website_configuration" "maintenance_mode" {
+  bucket = aws_s3_bucket.maintenance_mode.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "maintenance_static_page" {
+  bucket       = aws_s3_bucket.maintenance_mode.bucket
+  key          = "index.html"
+  source       = "./static_website/index.html"
+  content_type = "text/html"
+  etag         = filemd5("./static_website/index.html")
+}
+
+data "aws_iam_policy_document" "allow_cloudfront_to_access_static_website_in_s3" {
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        aws_cloudfront_origin_access_identity.maintenance_mode.iam_arn
+      ]
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.maintenance_mode.arn,
+      "${aws_s3_bucket.maintenance_mode.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudfront_to_access_static_website_in_s3" {
+  bucket = aws_s3_bucket.maintenance_mode.id
+  policy = data.aws_iam_policy_document.allow_cloudfront_to_access_static_website_in_s3.json
+}
