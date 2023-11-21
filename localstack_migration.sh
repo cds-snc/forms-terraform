0693ea43c5220572b121a094f8d8f45c2cbc9bdd
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-11-21 11:21:59 -0500

    update migration script

--- localstack_migration.sh
+++ localstack_migration.sh
@@ -15,27 +15,43 @@ reset='\033[0m' # No Color
 tgswitch 0.53.2
 tfswitch 1.6.4
 
-# Usage:
-# Without any args will reuse the existing cached packages saving some tiem and bandwidth
-# With the 'clean' argument will remove all cached packages for terraform and node modules for lambdas.
+ACTION=$1
+DRY_RUN="-dry-run"
+
+if [[ "$ACTION" == "for_real" ]]; then
+    DRY_RUN=""
+fi
 
 basedir=$(pwd)
 
+cd $basedir/env/cloud/app
+printf "${color}=> Removing S3 buckets from state${reset}\n"
+s3Buckets=(
+    aws_s3_bucket.archive_storage
+    aws_s3_bucket.lambda_code
+    aws_s3_bucket.reliability_file_storage
+    aws_s3_bucket.vault_file_storage
+)
 
-printf "${color}=> Listing Existing S3 buckets${reset}\n"
-cd $basedir/env/cloud/s3
+for i in "${s3Buckets[@]}"
+do
+    terragrunt state rm $DRY_RUN $i
+done
 
-terragrunt refresh | sed s'|: Refreshing state... \[id=| |' | sed s'|\]$||'
+printf "${color}=> Removing Cloudwatch logs from state${reset}\n"
 
-# printf "${color}=> Removing S3 buckets from state${reset}\n"
-# s3Buckets=(
-# aws_s3_bucket.archive_storage
-# aws_s3_bucket.lambda_code
-# aws_s3_bucket.reliability_file_storage
-# aws_s3_bucket.vault_file_storage
-# )
+cloudwatchLogs=(
+    aws_cloudwatch_log_group.reliability
+    aws_cloudwatch_log_group.submission
+    aws_cloudwatch_log_group.archiver
+    aws_cloudwatch_log_group.dead_letter_queue_consumer
+    aws_cloudwatch_log_group.archive_form_templates
+    aws_cloudwatch_log_group.audit_logs
+    aws_cloudwatch_log_group.nagware
+    aws_cloudwatch_log_group.vault_data_integrity_check
+)
 
-# for i in "${s3Buckets[@]}"
-# do
-#     terragrunt state rm $i
-# done
+for i in "${cloudwatchLogs[@]}"
+do
+    terragrunt state rm $DRY_RUN $i
+done
