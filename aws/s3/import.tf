5df7e3251b20cc682b5971af618b165e314c4351
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-11-21 10:31:50 -0500

    import s3 buckets

---
+++ aws/s3/import.tf
@@ -0,0 +1,14 @@
+import {
+    to = aws_s3_bucket.reliability_file_storage
+    id = "forms-${var.env}-reliability-file-storage"
+}
+
+import {
+    to = aws_s3_bucket.archive_storage
+    id = "forms-${var.env}-archive-storage"
+}
+
+import {
+    to = aws_s3_bucket.vault_file_storage
+    id = "forms-${var.env}-vault-file-storage"
+}
