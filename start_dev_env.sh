e09b801afbadb73ca5aaacde019ff40e574eff7d
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-11-28 14:06:28 -0500

    update dev start script

--- start_dev_env.sh
+++ start_dev_env.sh
@@ -2,4 +2,4 @@
 
 basedir=$(pwd)
 
-(trap 'kill 0' SIGINT; DYNAMODB_SHARE_DB=1 LS_LOG=warn localstack start & (sleep 10 && $basedir/localstack_services.sh))
+(trap 'kill -9' SIGINT; (sleep 10 && $basedir/localstack_services.sh) & DYNAMODB_SHARE_DB=1 LAMBDA_RUNTIME_ENVIRONMENT_TIMEOUT=60 LS_LOG=warn localstack start)
