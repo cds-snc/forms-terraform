675d544dba1b8d532bb544da45a791a9ad9b37e5
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-12-06 13:44:26 -0500

    remove start_dev_env

--- start_dev_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-basedir=$(pwd)
-
-(trap 'kill -9' SIGINT; (sleep 10 && $basedir/localstack_services.sh) & DYNAMODB_SHARE_DB=1 LAMBDA_RUNTIME_ENVIRONMENT_TIMEOUT=60 LS_LOG=warn localstack start)
