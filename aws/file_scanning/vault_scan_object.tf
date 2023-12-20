efce98a155c9aacd7c41ded71a3762db75d43f7b
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2023-12-20 14:59:09 -0500

    change module versiont to commit ref

--- aws/file_scanning/vault_scan_object.tf
+++ aws/file_scanning/vault_scan_object.tf
@@ -1,5 +1,5 @@
 module "vault_scan_object" {
-  source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=v6.1.3"
+  source = "github.com/cds-snc/terraform-modules//S3_scan_object?ref=bd904d01094f196fd3e8ff5c46e73838f1f1be26"
 
   s3_upload_bucket_name = var.vault_file_storage_id
   billing_tag_value     = var.billing_tag_value
