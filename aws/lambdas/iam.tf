f989a9f5206d03cdc2df0acfdc9a9547060dc3c3
Clément Janin <clement.janin@cds-snc.ca>
2024-01-03 15:33:36 -0500

    fix terraform formatting issue

--- aws/lambdas/iam.tf
+++ aws/lambdas/iam.tf
@@ -53,7 +53,7 @@ data "aws_iam_policy_document" "lambda_rds" {
   # checkov:skip=CKV_AWS_111: Write access without constraints is allowed
   # checkov:skip=CKV_AWS_356: Ensure no IAM policies documents allow "*" as a statement's resource for restrictable actions
   // TODO: refactor to remove `resources = ["*"]` (then we can remove checkov:skip=CKV_AWS_356)
-  
+
   statement {
     sid    = "RDSDataServiceAccess"
     effect = "Allow"
