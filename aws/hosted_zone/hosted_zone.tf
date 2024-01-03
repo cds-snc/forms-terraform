16479860e87b9a2d797fdccd4d61d637a283571d
Clément Janin <clement.janin@cds-snc.ca>
2024-01-03 12:09:27 -0500

    add checkov skip for hosted zone DNS query logging

--- aws/hosted_zone/hosted_zone.tf
+++ aws/hosted_zone/hosted_zone.tf
@@ -4,7 +4,8 @@
 #
 
 resource "aws_route53_zone" "form_viewer" {
-  # checkov:skip=CKV2_AWS_38: Domain Name System Security Extensions not required
+  # checkov:skip=CKV2_AWS_38: Domain Name System Security Extensions signing not required
+  # checkov:skip=CKV2_AWS_38: Domain Name System query logging not required
   count = length(var.domains)
   name  = var.domains[count.index]
 }
