82eda3a7824f6aced6b28bdc34a9a2443dee501a
Clément Janin <clement.janin@cds-snc.ca>
2024-01-03 11:51:27 -0500

    add checkov skip for hosted zone Domain Name System Security Extensions

--- aws/hosted_zone/hosted_zone.tf
+++ aws/hosted_zone/hosted_zone.tf
@@ -2,7 +2,9 @@
 # Route53 hosted zone:
 # Holds the DNS records for the service
 #
+
 resource "aws_route53_zone" "form_viewer" {
+  # checkov:skip=CKV2_AWS_38: Domain Name System Security Extensions not required
   count = length(var.domains)
   name  = var.domains[count.index]
 }
