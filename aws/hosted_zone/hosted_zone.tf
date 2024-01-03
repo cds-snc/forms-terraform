4c2203aed426e10a9effb6abfc85c7b9df2e5f5e
Clément Janin <clement.janin@cds-snc.ca>
2024-01-03 12:31:50 -0500

    fix wrong checkov number

--- aws/hosted_zone/hosted_zone.tf
+++ aws/hosted_zone/hosted_zone.tf
@@ -5,7 +5,7 @@
 
 resource "aws_route53_zone" "form_viewer" {
   # checkov:skip=CKV2_AWS_38: Domain Name System Security Extensions signing not required
-  # checkov:skip=CKV2_AWS_38: Domain Name System query logging not required
+  # checkov:skip=CKV2_AWS_39: Domain Name System query logging not required
   count = length(var.domains)
   name  = var.domains[count.index]
 }
