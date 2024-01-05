18b4efefad9fb09b0f7639925a57ff3c78e4e285
Clément Janin <clement.janin@cds-snc.ca>
2024-01-05 11:17:24 -0500

    fix terraform formatting issue

--- aws/hosted_zone/hosted_zone.tf
+++ aws/hosted_zone/hosted_zone.tf
@@ -8,7 +8,6 @@ resource "aws_route53_zone" "form_viewer" {
   # checkov:skip=CKV2_AWS_39: Domain Name System query logging not required
   // TODO: Implement Domain Name System Security Extensions signing
   // TODO: Implement Domain Name System query logging
-  
   count = length(var.domains)
   name  = var.domains[count.index]
 }
