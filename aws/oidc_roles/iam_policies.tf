0a38bb97bbac2f6210a1f74bd1fc10773f1775b3
Pat Heard <patrick.heard@cds-snc.ca>
2024-01-10 19:33:16 +0000

    fix: add platform-forms-client OIDC roles and policies

---
+++ aws/oidc_roles/iam_policies.tf
@@ -0,0 +1,146 @@
+#
+# Create and Manage PR review environment resources
+#
+resource "aws_iam_policy" "platform_forms_client_pr_review_env" {
+  count  = var.env == "staging" ? 1 : 0
+  name   = local.platform_forms_client_pr_review_env
+  path   = "/"
+  policy = data.aws_iam_policy_document.platform_forms_client_pr_review_env[0].json
+}
+
+data "aws_iam_policy_document" "platform_forms_client_pr_review_env" {
+  count = var.env == "staging" ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "lambda:AddPermission",
+      "lambda:CreateFunction",
+      "lambda:CreateFunctionUrlConfig",
+      "lambda:DeleteFunction",
+      "lambda:DeleteFunctionUrlConfig",
+      "lambda:DeleteFunctionConcurrency",
+      "lambda:GetFunction",
+      "lambda:GetFunctionConfiguration",
+      "lambda:GetFunctionUrlConfig",
+      "lambda:ListFunctionUrlConfigs",
+      "lambda:PutFunctionConcurrency",
+      "lambda:UpdateFunctionCode",
+      "lambda:UpdateFunctionConfiguration",
+      "lambda:UpdateFunctionUrlConfig"
+    ]
+    resources = [
+      "arn:aws:lambda:${var.region}:${var.account_id}:function:forms-client-pr-*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = [
+      "arn:aws:iam::${var.account_id}:role/forms-lambda-client"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:DescribeLogGroups"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DeleteLogGroup",
+      "logs:DeleteLogStream",
+      "logs:DeleteRetentionPolicy",
+      "logs:DescribeLogStreams",
+      "logs:PutRetentionPolicy"
+    ]
+    resources = [
+      "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/lambda/forms-client-pr-*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchDeleteImage",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:InitiateLayerUpload",
+      "ecr:ListImages",
+      "ecr:PutImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:UploadLayerPart"
+    ]
+    resources = [
+      "arn:aws:ecr:${var.region}:${var.account_id}:repository/pr_review"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+}
+
+#
+# Push and manage ECR images
+#
+resource "aws_iam_policy" "platform_forms_client_release" {
+  count  = var.env == "production" ? 1 : 0
+  name   = local.platform_forms_client_release
+  path   = "/"
+  policy = data.aws_iam_policy_document.platform_forms_client_release[0].json
+}
+
+data "aws_iam_policy_document" "platform_forms_client_release" {
+  count = var.env == "production" ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchDeleteImage",
+      "ecr:BatchGetImage",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:InitiateLayerUpload",
+      "ecr:ListImages",
+      "ecr:PutImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:UploadLayerPart"
+    ]
+    resources = [
+      "arn:aws:ecr:${var.region}:${var.account_id}:repository/form_viewer_production"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+}
--- aws/oidc_roles/iam_roles.tf
+++ aws/oidc_roles/iam_roles.tf
@@ -2,6 +2,8 @@ locals {
   forms_terraform_apply_release           = "forms-terraform-apply-release"
   forms_terraform_apply_workflow_dispatch = "forms-terraform-apply-workflow-dispatch"
   forms_terraform_plan_workflow_dispatch  = "forms-terraform-plan-workflow-dispatch"
+  platform_forms_client_pr_review_env     = "platform-forms-client-pr-review-env"
+  platform_forms_client_release           = "platform-forms-client-release"
 }
 
 # 
@@ -46,6 +48,16 @@ module "github_workflow_roles" {
       name      = local.forms_terraform_plan_workflow_dispatch
       repo_name = "forms-terraform"
       claim     = "workflow_dispatch"
+    },
+    {
+      name      = local.platform_forms_client_pr_review_env
+      repo_name = "platform-forms-client"
+      claim     = "pull_request"
+    },
+    {
+      name      = local.platform_forms_client_release
+      repo_name = "platform-forms-client"
+      claim     = "release"
     }
   ]
 }
@@ -85,6 +97,24 @@ resource "aws_iam_role_policy_attachment" "forms_terraform_plan_workflow_dispatc
   count      = var.env == "staging" ? 1 : 0
   role       = local.forms_terraform_plan_workflow_dispatch
   policy_arn = data.aws_iam_policy.readonly.arn
+  depends_on = [
+    module.github_workflow_roles
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "platform_forms_client_pr_review_env" {
+  count      = var.env == "staging" ? 1 : 0
+  role       = local.platform_forms_client_pr_review_env
+  policy_arn = aws_iam_policy.platform_forms_client_pr_review_env[0].arn
+  depends_on = [
+    module.github_workflow_roles
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "platform_forms_client_release" {
+  count      = var.env == "production" ? 1 : 0
+  role       = local.platform_forms_client_release
+  policy_arn = aws_iam_policy.platform_forms_client_release[0].arn
   depends_on = [
     module.github_workflow_roles
   ]
