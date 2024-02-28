54d064fb4c58c293f4e0d01ab54ceee19255af30
Wissam Moussa <wissam.moussa@cds-snc.ca>
2024-02-28 15:35:27 -0500

    use yarn and remove allowImportingTsExtensions

--- aws/alarms/lambda/notify_slack/notify_slack.test.ts
+++ aws/alarms/lambda/notify_slack/notify_slack.test.ts
@@ -1,5 +1,8 @@
+/*
+ * Since we haven't set allowImportingTsExtensions, you'll need to ensure that only one 'notify_slack' function exists in the path. Therefore, the 'dist' folder must not exist; otherwise, it will find a duplicate.
+ */
 import { CloudWatchLogsLogEvent } from "aws-lambda";
-import { handler } from "./notify_slack.ts";
+import { handler } from "notify_slack";
 
 describe("Unit test for app handler", function () {
   it("verifies successful response", async () => {
