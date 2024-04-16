4850e0bd99a5b5c5251a3eb485350eff5a46799f
Clément Janin <clement.janin@cds-snc.ca>
2024-04-16 09:05:56 -0400

    feat: add new ECR repositories for Lambda images

--- aws/ecr/ecr.tf
+++ aws/ecr/ecr.tf
@@ -8,8 +8,6 @@ resource "aws_ecr_repository" "viewer_repository" {
   image_scanning_configuration {
     scan_on_push = true
   }
-
-
 }
 
 resource "aws_ecr_lifecycle_policy" "form_viewer_policy" {
@@ -34,7 +32,382 @@ resource "aws_ecr_lifecycle_policy" "form_viewer_policy" {
 }
 
 #
-# Elastic Container Registry
+# Holds the containerized Lambda functions
+#
+
+resource "aws_ecr_repository" "audit_logs_lambda" {
+  name                 = "audit-logs-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "audit_logs_lambda" {
+  repository = aws_ecr_repository.audit_logs_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "audit_logs_archiver_lambda" {
+  name                 = "audit-logs-archiver-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "audit_logs_archiver_lambda" {
+  repository = aws_ecr_repository.audit_logs_archiver_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "form_archiver_lambda" {
+  name                 = "form-archiver-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "form_archiver_lambda" {
+  repository = aws_ecr_repository.form_archiver_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "nagware_lambda" {
+  name                 = "nagware-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "nagware_lambda" {
+  repository = aws_ecr_repository.nagware_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "reliability_lambda" {
+  name                 = "reliability-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "reliability_lambda" {
+  repository = aws_ecr_repository.reliability_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "reliability_dlq_consumer_lambda" {
+  name                 = "reliability-dlq-consumer-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "reliability_dlq_consumer_lambda" {
+  repository = aws_ecr_repository.reliability_dlq_consumer_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "response_archiver_lambda" {
+  name                 = "response-archiver-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "response_archiver_lambda" {
+  repository = aws_ecr_repository.response_archiver_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "submission_lambda" {
+  name                 = "submission-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "submission_lambda" {
+  repository = aws_ecr_repository.submission_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "vault_integrity_lambda" {
+  name                 = "vault-integrity-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "vault_integrity_lambda" {
+  repository = aws_ecr_repository.vault_integrity_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "notify_slack_lambda" {
+  name                 = "notify-slack-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "notify_slack_lambda" {
+  repository = aws_ecr_repository.notify_slack_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "cognito_email_sender_lambda" {
+  name                 = "cognito-email-sender-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "cognito_email_sender_lambda" {
+  repository = aws_ecr_repository.cognito_email_sender_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+resource "aws_ecr_repository" "cognito_pre_sign_up_lambda" {
+  name                 = "cognito-pre-sign-up-lambda"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "cognito_pre_sign_up_lambda" {
+  repository = aws_ecr_repository.cognito_pre_sign_up_lambda.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 30
+      }
+
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
+
+#
 # Holds the Load Test image used by the Lambda
 #
 resource "aws_ecr_repository" "load_test_repository" {
--- aws/ecr/outputs.tf
+++ aws/ecr/outputs.tf
@@ -3,6 +3,66 @@ output "ecr_repository_url_form_viewer" {
   value       = aws_ecr_repository.viewer_repository.repository_url
 }
 
+output "ecr_repository_url_audit_logs_lambda" {
+  description = "URL of the Audit Logs Lambda ECR"
+  value       = aws_ecr_repository.audit_logs_lambda.repository_url
+}
+
+output "ecr_repository_url_audit_logs_archiver_lambda" {
+  description = "URL of the Audit Logs Archiver Lambda ECR"
+  value       = aws_ecr_repository.audit_logs_archiver_lambda.repository_url
+}
+
+output "ecr_repository_url_form_archiver_lambda" {
+  description = "URL of the Form Archiver Lambda ECR"
+  value       = aws_ecr_repository.form_archiver_lambda.repository_url
+}
+
+output "ecr_repository_url_nagware_lambda" {
+  description = "URL of the Nagware Lambda ECR"
+  value       = aws_ecr_repository.nagware_lambda.repository_url
+}
+
+output "ecr_repository_url_reliability_lambda" {
+  description = "URL of the Reliability Lambda ECR"
+  value       = aws_ecr_repository.reliability_lambda.repository_url
+}
+
+output "ecr_repository_url_reliability_dlq_consumer_lambda" {
+  description = "URL of the Reliability DLQ Consumer Lambda ECR"
+  value       = aws_ecr_repository.reliability_dlq_consumer_lambda.repository_url
+}
+
+output "ecr_repository_url_response_archiver_lambda" {
+  description = "URL of the Response Archiver Lambda ECR"
+  value       = aws_ecr_repository.response_archiver_lambda.repository_url
+}
+
+output "ecr_repository_url_submission_lambda" {
+  description = "URL of the Submission Lambda ECR"
+  value       = aws_ecr_repository.submission_lambda.repository_url
+}
+
+output "ecr_repository_url_vault_integrity_lambda" {
+  description = "URL of the Vault Integrity Lambda ECR"
+  value       = aws_ecr_repository.vault_integrity_lambda.repository_url
+}
+
+output "ecr_repository_url_notify_slack_lambda" {
+  description = "URL of the Notify Slack Lambda ECR"
+  value       = aws_ecr_repository.notify_slack_lambda.repository_url
+}
+
+output "ecr_repository_url_cognito_email_sender_lambda" {
+  description = "URL of the Cognito Email Sender Lambda ECR"
+  value       = aws_ecr_repository.cognito_email_sender_lambda.repository_url
+}
+
+output "ecr_repository_url_cognito_pre_sign_up_lambda" {
+  description = "URL of the Cognito Pre Sign Up Lambda ECR"
+  value       = aws_ecr_repository.cognito_pre_sign_up_lambda.repository_url
+}
+
 output "ecr_repository_url_load_test" {
   description = "URL of the Form viewer ECR"
   value       = length(aws_ecr_repository.load_test_repository) > 0 ? aws_ecr_repository.load_test_repository[0].repository_url : ""
