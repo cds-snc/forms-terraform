2173f16e5e2e7ce65f187b66a91db5f7c6a4bc41
Pat Heard <patrick.heard@cds-snc.ca>
2024-06-20 15:34:57 +0000

    fix: reduce to maxium evaluation period of CloudWatch

--- aws/alarms/locals.tf
+++ aws/alarms/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  lambda_submission_expect_invocation_in_period = var.env == "production" ? var.lambda_submission_expect_invocation_in_period : 60 * 24 * 7 # expect once a week in non-prod envs
+  lambda_submission_expect_invocation_in_period = var.env == "production" ? var.lambda_submission_expect_invocation_in_period : 60 * 24 # expect once a day in non-prod envs
 }
