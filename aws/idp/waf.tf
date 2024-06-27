461a5dfe523db5df56fea479ffb9aedbf10f8075
Pat Heard <patrick.heard@cds-snc.ca>
2024-06-27 20:10:50 +0000

    fix: add missing limit request group

--- aws/idp/waf.tf
+++ aws/idp/waf.tf
@@ -100,6 +100,77 @@ resource "aws_wafv2_web_acl" "idp" {
   tags = local.common_tags
 }
 
+resource "aws_wafv2_rule_group" "rate_limiters_group" {
+  capacity = 32 // 2, as a base cost. For each custom aggregation key that you specify, add 30 WCUs.
+  name     = "RateLimitersGroup"
+  scope    = "REGIONAL"
+
+  rule {
+    name     = "BlanketRequestLimit"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 5000
+        aggregate_key_type = "IP"
+
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "BlanketRequestLimit"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  rule {
+    name     = "PostRequestLimit"
+    priority = 2
+
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        limit              = 1000
+        aggregate_key_type = "IP"
+        scope_down_statement {
+          byte_match_statement {
+            positional_constraint = "EXACTLY"
+            field_to_match {
+              method {}
+            }
+            search_string = "post"
+            text_transformation {
+              priority = 1
+              type     = "LOWERCASE"
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "PostRequestRateLimit"
+      sampled_requests_enabled   = true
+    }
+  }
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "RateLimitersGroup"
+    sampled_requests_enabled   = false
+  }
+
+  tags = local.common_tags
+}
+
 resource "aws_wafv2_web_acl_association" "idp" {
   resource_arn = aws_lb.idp.arn
   web_acl_arn  = aws_wafv2_web_acl.idp.arn
