7ccb3c324377f9435c9cd83ad8a5a5656d1c80a0
Pat Heard <patrick.heard@cds-snc.ca>
2024-07-21 11:54:25 +0000

    chore: upgrade to latest Terraform and Terragrunt
    Upgrade to the latest versions of both tools.

--- localstack_migration.sh
+++ localstack_migration.sh
@@ -12,8 +12,8 @@ color='\033[1;95m'
 reset='\033[0m' # No Color
 # Set proper terraform and terragrunt versions
 
-tgswitch 0.54.8
-tfswitch 1.6.6
+tgswitch 0.63.2
+tfswitch 1.9.2
 
 ACTION=$1
 DRY_RUN="-dry-run"
