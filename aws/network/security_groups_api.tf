e127f66a80d6fab9dfde706c3de039c4e8206e41
Pat Heard <patrick.heard@cds-snc.ca>
2024-08-29 08:46:10 -0400

    feat: allow API to access Redis (#790)
    Update the the security groups to allow the API to connect
    to the ElastiCache Redis cluster.

--- aws/network/security_groups_api.tf
+++ aws/network/security_groups_api.tf
@@ -80,3 +80,28 @@ resource "aws_security_group_rule" "api_ecs_egress_db" {
   security_group_id        = aws_security_group.api_ecs[0].id
   source_security_group_id = aws_security_group.forms_database.id
 }
+
+# Redis
+resource "aws_security_group_rule" "redis_ingress_api_ecs" {
+  count = var.feature_flag_api ? 1 : 0
+
+  description              = "Ingress to Redis from API ECS task"
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.forms_redis.id
+  source_security_group_id = aws_security_group.api_ecs[0].id
+}
+
+resource "aws_security_group_rule" "api_ecs_egress_redis" {
+  count = var.feature_flag_api ? 1 : 0
+
+  description              = "Egress from API ECS task to Redis"
+  type                     = "egress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.api_ecs[0].id
+  source_security_group_id = aws_security_group.forms_redis.id
+}
