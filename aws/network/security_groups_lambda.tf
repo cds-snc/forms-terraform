dab667f6f8b048df46170bcd4e84541586cdbe4b
Pat Heard <patrick.heard@cds-snc.ca>
2024-09-06 12:27:33 +0000

    chore: move Athenat connector SG into new Lambda file

--- aws/network/security_groups_lambda.tf
+++ aws/network/security_groups_lambda.tf
@@ -1,9 +1,13 @@
+#
+# Nagware
+#
 resource "aws_security_group" "lambda_nagware" {
   description = "Lambda Nagware"
   name        = "lambda_nagware"
   vpc_id      = aws_vpc.forms.id
 }
 
+# Internet
 resource "aws_security_group_rule" "lambda_nagware_egress_internet" {
   description       = "Egress to the internet from Nagware Lambda function"
   type              = "egress"
@@ -12,6 +16,17 @@ resource "aws_security_group_rule" "lambda_nagware_egress_internet" {
   protocol          = "tcp"
   security_group_id = aws_security_group.lambda_nagware.id
   cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# PrivateLink
+resource "aws_security_group_rule" "privatelink_lambda_nagware_ingress" {
+  description              = "Security group rule for Nagware Lambda function ingress"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.privatelink.id
+  source_security_group_id = aws_security_group.lambda_nagware.id
 }
 
 # Redis
@@ -33,4 +48,67 @@ resource "aws_security_group_rule" "lambda_nagware_egress_redis" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.lambda_nagware.id
   source_security_group_id = aws_security_group.forms_redis.id
+}
+
+#
+# Athena connector
+#
+resource "aws_security_group" "connector_db" {
+  name        = "connector_db"
+  description = "For the Lambda RDS connector"
+  vpc_id      = aws_vpc.forms.id
+}
+
+# Database
+resource "aws_security_group_rule" "connector_ingress_rds" {
+  description              = "Ingress to database from lambda connector"
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.forms_database.id
+  source_security_group_id = aws_security_group.connector_db.id
+}
+
+resource "aws_security_group_rule" "connector_egress_rds" {
+  description              = "Egress from lambda connector to database"
+  type                     = "egress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.connector_db.id
+  source_security_group_id = aws_security_group.forms_database.id
+}
+
+# PrivateLink
+resource "aws_security_group_rule" "connector_db_egress_privatelink" {
+  description              = "Egress from lambda connector to PrivateLink endpoints"
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.connector_db.id
+  source_security_group_id = aws_security_group.privatelink.id
+}
+
+resource "aws_security_group_rule" "privatelink_connector_db_ingress" {
+  description              = "Security group rule for lambda connector ingress"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.privatelink.id
+  source_security_group_id = aws_security_group.connector_db.id
+}
+
+resource "aws_security_group_rule" "s3_gateway_connector_db_egress" {
+  description       = "Security group rule for Lambda RDS Connector S3 egress through VPC endpoints"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.connector_db.id
+  prefix_list_ids = [
+    aws_vpc_endpoint.s3.prefix_list_id
+  ]
 }
