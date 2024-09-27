7444d258123d0b1dfe7c181c8e0637b5f425bbc0
Pat Heard <patrick.heard@cds-snc.ca>
2024-09-27 15:29:53 -0400

    fix: adjust IdP CloudWatch alarm error filter (#849)
    Update the IdP's CloudWatch alarm filter pattern so that it includes logs with a 
    capitalized ERROR term, while also omitting access token requests where the 
    user has signed the JWT with an invalid private key.

--- aws/alarms/locals.tf
+++ aws/alarms/locals.tf
@@ -1,8 +1,14 @@
 locals {
   # Define the pattern that will be used to detect errors in the IdP logs
   # Any log message that contains a word from `idp_error` and does not contain a word from `idp_error_ignore` will be detected
-  idp_error         = ["level=error"]
-  idp_error_ignore  = ["context canceled"]
+  idp_error = [
+    "level=error",
+    "level=ERROR"
+  ]
+  idp_error_ignore = [
+    "context canceled",        # user cancels request before it completes
+    "Errors.AuthNKey.NotFound" # user requests an access token with an invalid key
+  ]
   idp_error_pattern = "[(w1=\"*${join("*\" || w1=\"*", local.idp_error)}*\") && w1!=\"*${join("*\" && w1!=\"*", local.idp_error_ignore)}*\"]"
 
   lambda_submission_expect_invocation_in_period = var.env == "production" ? var.lambda_submission_expect_invocation_in_period : 60 * 24 # expect once a day in non-prod envs
