8aca4ce8921d1330d0de2b17c9302f8ea43496ee
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-11-28 14:46:27 -0500

    give reliability lambda access to rds

--- aws/lambdas/reliability.tf
+++ aws/lambdas/reliability.tf
@@ -5,6 +5,12 @@ resource "aws_lambda_function" "reliability" {
   role          = aws_iam_role.lambda.arn
   timeout       = 300
 
+  vpc_config {
+    security_group_ids = [var.lambda_security_group_id]
+    subnet_ids         = var.private_subnet_ids
+  }
+
+
   lifecycle {
     ignore_changes = [image_uri]
   }
