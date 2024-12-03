f1952eb2c9c03317a2d52142e5a2ff9715942a09
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2024-12-03 13:59:23 -0500

    adding back legacy sec groups

--- aws/network/security_groups_lambda.tf
+++ aws/network/security_groups_lambda.tf
@@ -1,3 +1,56 @@
+### Everything below this line needs to be deleted
+resource "aws_security_group" "lambda_nagware" {
+  description = "Lambda Nagware"
+  name        = "lambda_nagware"
+  vpc_id      = aws_vpc.forms.id
+}
+
+# Internet
+resource "aws_security_group_rule" "lambda_nagware_egress_internet" {
+  description       = "Egress to the internet from Nagware Lambda function"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.lambda_nagware.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# PrivateLink
+resource "aws_security_group_rule" "privatelink_lambda_nagware_ingress" {
+  description              = "Security group rule for Nagware Lambda function ingress"
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.privatelink.id
+  source_security_group_id = aws_security_group.lambda_nagware.id
+}
+
+# Redis
+resource "aws_security_group_rule" "redis_ingress_lambda_nagware" {
+  description              = "Ingress to Redis from Nagware Lambda function"
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.forms_redis.id
+  source_security_group_id = aws_security_group.lambda_nagware.id
+}
+
+resource "aws_security_group_rule" "lambda_nagware_egress_redis" {
+  description              = "Egress from Nagware Lambda function to Redis"
+  type                     = "egress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.lambda_nagware.id
+  source_security_group_id = aws_security_group.forms_redis.id
+
+}
+### Everything above this line needs to be deleted
+
+
 #
 # Nagware
 #
