cf129dbf43bc0574f50a30b0bbb33b3d3705e63e
Vivian Nobrega <vivian.nobrega@cds-snc.ca>
2025-01-09 13:32:04 -0500

    * add comments to explain etl job more.

--- aws/glue/scripts/rds_etl.py
+++ aws/glue/scripts/rds_etl.py
@@ -9,7 +9,8 @@ from pyspark.sql.functions import col, from_unixtime, date_format, lit, current_
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType, BooleanType
 from datetime import datetime
 
-# Initialize Glue context
+# ------- Step 1 -------
+# Initialize Glue context, and logger.
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'rds_endpoint', 'rds_db_name', 'rds_username', 'rds_password', 'rds_bucket', 's3_endpoint'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -18,9 +19,10 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 logger = glueContext.get_logger()
 
-# print the arguments
+# Log the ETL Job has begun
 logger.info("Starting Script for ETL of RDS data")
 
+# ------- Step 2 -------
 # Load data from source
 datasource0 = glueContext.create_dynamic_frame.from_options(
     connection_type = "postgresql",
@@ -33,6 +35,9 @@ datasource0 = glueContext.create_dynamic_frame.from_options(
     transformation_ctx = "datasource0"
 )
 
+# ------- Step 3 -------
+# Process the data (easy-mode)
+
 # Get current timestamp
 current_stamp = current_timestamp()
 
@@ -44,8 +49,10 @@ redacted_df = redacted_df.withColumn("updated_at", date_format(from_unixtime(col
 # Add a timestamp column for Athena to use as a partition.
 redacted_df = redacted_df.withColumn("timestamp", date_format(from_unixtime(current_stamp.cast("bigint")), "yyyy-MM-dd HH:mm:ss.SSSSSSSSS"))
 
-# ---- Parse the jsonConfig column to extract the values ----
-# Add Substructures
+# ------- Step 4 -------
+# Parse the jsonConfig column to extract the values
+
+# Add Substructures, these contain the strongly typed data that the final table will contain.
 sub_element_schema = StructType([
     StructField("id", IntegerType(), True),
     StructField("type", StringType(), True),
@@ -68,18 +75,20 @@ json_schema = StructType([
      StructField("elements", ArrayType(element_schema), True),
 ])
 
-# Perform json parse
+# Perform json parse for basic elements.
 redacted_df = redacted_df.withColumn("parsed_json", from_json(col("jsonConfig"), json_schema))
 redacted_df = redacted_df.withColumn("titleEn", col("parsed_json.titleEn"))
 redacted_df = redacted_df.withColumn("titleFr", col("parsed_json.titleFr"))
 
-# handle the array of elements
+# ------- Step 4.1 -------
+# handle the array of elements and subElements
 elements_df = (
     redacted_df.select(
         col("id"),
         explode(col("parsed_json.elements")).alias("element")
     )
-)
+) # explode the elements array
+
 top_level_types_df = (
     elements_df
     .select(
@@ -87,15 +96,15 @@ top_level_types_df = (
         col("element.type").alias("element_type")
     )
     .withColumn("count_value", lit(1))
-)
+) # select the top level types
+
 sub_elements_df = (
     elements_df
     .select(
         col("id"),
         explode_outer(col("element.properties.subElements")).alias("subElement")
     )
-    # If subElements can be null, consider explode_outer or handle null arrays.
-)
+) # explode the subElements array (if it exists)
 
 sub_types_df = (
     sub_elements_df
@@ -104,16 +113,16 @@ sub_types_df = (
         col("subElement.type").alias("element_type")
     )
     .withColumn("count_value", lit(1))
-)
+) # select the subElement types
 
-all_types_df = top_level_types_df.unionByName(sub_types_df)
+all_types_df = top_level_types_df.unionByName(sub_types_df) # combine the top level and subElement types
 
 grouped_counts_df = (
     all_types_df
     .filter(col("element_type").isNotNull()) # filter out null types (eg: form subelements and elements that don't have an 'element_type')
     .groupBy("id", "element_type")
     .agg(spark_sum("count_value").alias("type_count"))
-)
+) # group by id and element_type and sum the counts
 
 pivoted_df = (
     grouped_counts_df
@@ -121,18 +130,23 @@ pivoted_df = (
     .pivot("element_type")
     .sum("type_count")
     .na.fill(0)  # fill missing type columns with 0
-)
+) # pivot the data to have one row per id
 
+# ------- Step 4.2 -------
+# rename the columns to have _count suffix
 renamed_cols_df = pivoted_df
 for c in renamed_cols_df.columns:
     if c != "id":
         renamed_cols_df = renamed_cols_df.withColumnRenamed(c, f"{c}_count")
 
+# ------- Step 4.3 -------
+# join the redacted_df with the renamed_cols_df
 redacted_df = (
     redacted_df
     .join(renamed_cols_df, on="id", how="left")
 )
 
+# ------- Step 5 -------
 # drop the jsonConfig and parsed_json columns
 redacted_df = redacted_df.drop("jsonConfig", "parsed_json")
 
@@ -142,6 +156,7 @@ final_df = DynamicFrame.fromDF(redacted_df, glueContext, "final_df")
 
 logger.info("Produced Final Dynamic Frame")
 
+# ------- Step 6 -------
 # Write the processed data to the target
 datasink4 = glueContext.write_dynamic_frame.from_options(
     frame = final_df,
@@ -153,4 +168,6 @@ datasink4 = glueContext.write_dynamic_frame.from_options(
 
 logger.info("Data written to S3")
 
+# ------- Step 7 -------
+# Job is comitted!~  Hooray
 job.commit()
