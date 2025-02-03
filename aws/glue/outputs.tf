2aea4b6adde89d63d9d27633b5c7940826746f00
Pat Heard <patrick.heard@cds-snc.ca>
2025-02-03 11:05:18 -0500

    fix: add missing Glue ETL log group output

--- aws/glue/outputs.tf
+++ aws/glue/outputs.tf
@@ -3,6 +3,11 @@ output "glue_crawler_log_group_name" {
   value       = local.glue_crawler_log_group_name
 }
 
+output "glue_etl_log_group_name" {
+  description = "The name of the Glue ETL CloudWatch log group."
+  value       = local.glue_etl_log_group_name
+}
+
 output "glue_database_name" {
   description = "The name of the Glue database."
   value       = aws_glue_catalog_database.rds_db_catalog.name
