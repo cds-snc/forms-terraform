03f59d6a656c6e3b02dd0ae4e3babb4b728d74c3
Pat Heard <patrick.heard@cds-snc.ca>
2025-02-04 14:31:46 -0500

    fix: restrict Glue job internet egress traffic

--- aws/network/security_groups_glue.tf
+++ aws/network/security_groups_glue.tf
@@ -5,14 +5,24 @@ resource "aws_security_group" "glue_job" {
   vpc_id      = aws_vpc.forms.id
 }
 
-resource "aws_security_group_rule" "glue_job_egress_internet" {
-  description       = "Egress from Glue jobs to internet (HTTPS)"
+resource "aws_security_group_rule" "glue_job_egress_s3" {
+  description       = "Egress from Glue jobs to S3"
   type              = "egress"
   to_port           = 443
   from_port         = 443
   protocol          = "tcp"
   security_group_id = aws_security_group.glue_job.id
-  cidr_blocks       = ["0.0.0.0/0"]
+  prefix_list_ids   = [aws_vpc_endpoint.s3.prefix_list_id]
+}
+
+resource "aws_security_group_rule" "glue_job_egress_self" {
+  description       = "Egress to Glue jobs from self on all ports.  This is a requirement for VPC connected Glue jobs."
+  type              = "egress"
+  to_port           = 65535
+  from_port         = 0
+  protocol          = "tcp"
+  security_group_id = aws_security_group.glue_job.id
+  self              = true
 }
 
 resource "aws_security_group_rule" "glue_job_ingress_self" {
