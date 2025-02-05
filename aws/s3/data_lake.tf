816355840a1a20315ff2c3a1a0628d0bf192b98b
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-02-05 15:05:58 -0500

    add back log bucket for data lake

--- aws/s3/data_lake.tf
+++ aws/s3/data_lake.tf
@@ -42,3 +42,19 @@ module "lake_bucket" {
     enabled = true
   }
 }
+
+#
+# Bucket access logs, stored for 30 days
+#
+module "log_bucket" {
+  source            = "github.com/cds-snc/terraform-modules//S3_log_bucket?ref=17994187b8628dc5decf74ead84768501378df4c" # ref for v10.0.0
+  bucket_name       = "cds-forms-data-lake-bucket-logs-${var.env}"
+  versioning_status = "Enabled"
+
+  lifecycle_rule = [
+    local.lifecycle_expire_all,
+    local.lifecycle_remove_noncurrent_versions
+  ]
+
+  billing_tag_value = var.billing_tag_value
+}
