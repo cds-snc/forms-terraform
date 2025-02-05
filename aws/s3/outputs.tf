9bd2c15aceecbee704b90758d2556b741e46af43
Clément Janin <clement.janin@cds-snc.ca>
2025-02-05 15:01:46 -0500

    removes unused output variables

--- aws/s3/outputs.tf
+++ aws/s3/outputs.tf
@@ -57,13 +57,3 @@ output "etl_bucket_name" {
   description = "Name of the S3 ETL bucket."
   value       = module.etl_bucket.s3_bucket_id
 }
-
-output "athena_bucket_arn" {
-  description = "ARN of the S3 Athena query result bucket."
-  value       = module.athena_bucket.s3_bucket_arn
-}
-
-output "athena_bucket_name" {
-  description = "Name of the S3 Athena query result bucket."
-  value       = module.athena_bucket.s3_bucket_id
-}
