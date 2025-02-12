43cfb73946c94330b703883e60f9669073723769
Pat Heard <patrick.heard@cds-snc.ca>
2025-02-12 13:59:54 -0500

    chore: temporarily disable data replication to data lake
    Disable the S3 replication rule to the Platform data lake until the ETL
    job testing is complete.

--- aws/glue/s3.tf
+++ aws/glue/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket_replication_configuration" "forms_s3_replicate_to_platfo
 
   rule {
     id       = "send-to-platform-data-lake"
-    status   = var.env == "production" ? "Enabled" : "Disabled"
+    status   = "Disabled" # Temporarily disabled until testing is complete
     priority = 10
 
     destination {
