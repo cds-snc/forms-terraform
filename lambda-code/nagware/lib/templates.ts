5cd34ffa3f1b39f38489cb1fece76b7d15e4cc1c
Clément JANIN <clement.janin@cds-snc.ca>
2025-02-20 14:39:15 -0500

    fix: revert localstack detection when initializing Postgres connector (#956)

--- lambda-code/nagware/lib/templates.ts
+++ lambda-code/nagware/lib/templates.ts
@@ -14,7 +14,7 @@ export async function getTemplateInfo(formID: string): Promise<TemplateInfo> {
     const postgresConnector =
       await PostgresConnector.defaultUsingPostgresConnectionUrlFromAwsSecret(
         process.env.DB_URL ?? "",
-        Boolean(process.env.LOCALSTACK)
+        process.env.LOCALSTACK === "true"
       );
 
     // Due to Localstack limitations we have to define aliases for fields that have the same name
