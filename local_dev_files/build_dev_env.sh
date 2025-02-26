2ad917fc584af8dbb48e5a9d359823cd58f54a66
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-02-26 11:27:04 -0500

    scale back max rds capacity

--- local_dev_files/build_dev_env.sh
+++ local_dev_files/build_dev_env.sh
@@ -118,4 +118,4 @@ DB_SECRET_ARN=$(aws secretsmanager list-secrets --filter Key="name",Values="serv
 DATABASE_URL=$(aws secretsmanager get-secret-value --secret-id $DB_SECRET_ARN --query "SecretString" --output text)
 REDIS_URL=$(aws elasticache describe-cache-clusters --show-cache-node-info --query "CacheClusters[0].CacheNodes[0].Endpoint.Address" --output text)
 RELIABILITY_FILE_STORAGE="forms-${AWS_ACCOUNT_ID}-reliability-file-storage"
-printf "${greenColor}=> Please copy the following to your app .env file:${reset}\nDATABASE_URL=${DATABASE_URL}?connect_timeout=60\nREDIS_URL=${REDIS_URL}:6379\nRELIABILITY_FILE_STORAGE=${RELIABILITY_FILE_STORAGE}\n"
+printf "${greenColor}=> Please copy the following to your app .env file:${reset}\nDATABASE_URL=${DATABASE_URL}?connect_timeout=30&pool_timeout=30\nREDIS_URL=${REDIS_URL}:6379\nRELIABILITY_FILE_STORAGE=${RELIABILITY_FILE_STORAGE}\n"
