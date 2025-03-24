4740c076173310d17a3c0d61e7d08f6a5d14a7ba
Clément Janin <clement.janin@cds-snc.ca>
2025-03-24 13:23:09 -0400

    feat: destroy PR review environment resources that will be shared with Rainbow deployment feature

--- aws/pr_review/security_groups.tf
+++ aws/pr_review/security_groups.tf
@@ -3,14 +3,16 @@
 # with Redis, DB, and receive recieve HTTPS requests.
 #
 resource "aws_security_group" "lambda_client_pr_review" {
-  count       = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   name        = "lambda-admin-pr-review"
   description = "Lambda admin PR review environment"
   vpc_id      = var.vpc_id
 }
 
 resource "aws_security_group_rule" "internet_ingress_to_lambda_client" {
-  count             = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description       = "Allow inbound connections from the internet to the lambda PR review env"
   type              = "ingress"
   from_port         = 443
@@ -21,7 +23,8 @@ resource "aws_security_group_rule" "internet_ingress_to_lambda_client" {
 }
 
 resource "aws_security_group_rule" "lambda_client_egress_to_internet" {
-  count             = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description       = "Allow outbound connections from lambda PR review env to the internet"
   type              = "egress"
   from_port         = 443
@@ -32,7 +35,8 @@ resource "aws_security_group_rule" "lambda_client_egress_to_internet" {
 }
 
 resource "aws_security_group_rule" "lambda_client_egress_privatelink" {
-  count                    = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description              = "Security group rule for Forms egress through privatelink"
   type                     = "egress"
   from_port                = 443
@@ -43,7 +47,8 @@ resource "aws_security_group_rule" "lambda_client_egress_privatelink" {
 }
 
 resource "aws_security_group_rule" "privatelink_lambda_client_ingress" {
-  count                    = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description              = "Security group rule for Forms ingress"
   type                     = "ingress"
   from_port                = 443
@@ -54,7 +59,8 @@ resource "aws_security_group_rule" "privatelink_lambda_client_ingress" {
 }
 
 resource "aws_security_group_rule" "lambda_client_egress_database" {
-  count                    = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description              = "Security group rule for Forms DB egress to Database"
   type                     = "egress"
   from_port                = 5432
@@ -65,7 +71,8 @@ resource "aws_security_group_rule" "lambda_client_egress_database" {
 }
 
 resource "aws_security_group_rule" "database_lambda_client_ingress" {
-  count                    = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description              = "Security group rule for Forms DB ingress"
   type                     = "ingress"
   from_port                = 5432
@@ -76,7 +83,8 @@ resource "aws_security_group_rule" "database_lambda_client_ingress" {
 }
 
 resource "aws_security_group_rule" "lambda_client_egress_redis" {
-  count                    = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description              = "Security group rule for Forms DB egress to Redis"
   type                     = "egress"
   from_port                = 6379
@@ -87,7 +95,8 @@ resource "aws_security_group_rule" "lambda_client_egress_redis" {
 }
 
 resource "aws_security_group_rule" "redis_lambda_client_ingress" {
-  count                    = var.env == "staging" ? 1 : 0
+  count = var.env == "staging" ? 1 : 0
+
   description              = "Security group rule for Forms Redis ingress"
   type                     = "ingress"
   from_port                = 6379
