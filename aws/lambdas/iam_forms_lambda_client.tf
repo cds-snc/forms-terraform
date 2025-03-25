6ea93b9be09ee9dbfd5c3788792c4fee7ec79426
Clément JANIN <clement.janin@cds-snc.ca>
2025-03-25 14:47:40 -0400

    fix: skip lambda IAM role creation in scratch account (#979)

--- aws/lambdas/iam_forms_lambda_client.tf
+++ aws/lambdas/iam_forms_lambda_client.tf
@@ -1,9 +1,13 @@
 resource "aws_iam_role" "forms_lambda_client" {
+  count = var.env == "development" ? 0 : 1
+
   name               = "forms-lambda-client"
-  assume_role_policy = data.aws_iam_policy_document.forms_lambda_client.json
+  assume_role_policy = data.aws_iam_policy_document.forms_lambda_client[0].json
 }
 
 data "aws_iam_policy_document" "forms_lambda_client" {
+  count = var.env == "development" ? 0 : 1
+
   statement {
     actions = ["sts:AssumeRole"]
     effect  = "Allow"
@@ -16,43 +20,59 @@ data "aws_iam_policy_document" "forms_lambda_client" {
 }
 
 resource "aws_iam_role_policy_attachment" "secrets_manager_forms_lambda_client" {
-  role       = aws_iam_role.forms_lambda_client.name
+  count = var.env == "development" ? 0 : 1
+
+  role       = aws_iam_role.forms_lambda_client[0].name
   policy_arn = var.ecs_iam_forms_secrets_manager_policy_arn
 }
 
 resource "aws_iam_role_policy_attachment" "kms_forms_lambda_client" {
-  role       = aws_iam_role.forms_lambda_client.name
+  count = var.env == "development" ? 0 : 1
+
+  role       = aws_iam_role.forms_lambda_client[0].name
   policy_arn = var.ecs_iam_forms_kms_policy_arn
 }
 
 resource "aws_iam_role_policy_attachment" "s3_forms_lambda_client" {
-  role       = aws_iam_role.forms_lambda_client.name
+  count = var.env == "development" ? 0 : 1
+
+  role       = aws_iam_role.forms_lambda_client[0].name
   policy_arn = var.ecs_iam_forms_s3_policy_arn
 }
 
 resource "aws_iam_role_policy_attachment" "dynamodb_forms_lambda_client" {
-  role       = aws_iam_role.forms_lambda_client.name
+  count = var.env == "development" ? 0 : 1
+
+  role       = aws_iam_role.forms_lambda_client[0].name
   policy_arn = var.ecs_iam_forms_dynamodb_policy_arn
 }
 
 resource "aws_iam_role_policy_attachment" "sqs_forms_lambda_client" {
-  role       = aws_iam_role.forms_lambda_client.name
+  count = var.env == "development" ? 0 : 1
+
+  role       = aws_iam_role.forms_lambda_client[0].name
   policy_arn = var.ecs_iam_forms_sqs_policy_arn
 }
 
 resource "aws_iam_role_policy_attachment" "cognito_forms_lambda_client" {
-  role       = aws_iam_role.forms_lambda_client.name
+  count = var.env == "development" ? 0 : 1
+
+  role       = aws_iam_role.forms_lambda_client[0].name
   policy_arn = var.ecs_iam_forms_cognito_policy_arn
 }
 
 resource "aws_iam_role_policy_attachment" "forms_lambda_client_vpc_access" {
-  role       = aws_iam_role.forms_lambda_client.name
+  count = var.env == "development" ? 0 : 1
+
+  role       = aws_iam_role.forms_lambda_client[0].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
 resource "aws_lambda_permission" "submission_lambda_invoke" {
+  count = var.env == "development" ? 0 : 1
+
   statement_id  = "AllowSubmissionInvokeFromFormsLambdaClient"
   action        = "lambda:InvokeFunction"
-  principal     = aws_iam_role.forms_lambda_client.arn
+  principal     = aws_iam_role.forms_lambda_client[0].arn
   function_name = aws_lambda_function.submission.function_name
 }
