263cd65fac2c539fe6bd5ffd7895129b5be40317
Clément Janin <clement.janin@cds-snc.ca>
2025-05-09 15:21:03 -0400

    fix: local communication issues between API end to end test lambda and IdP + API services

--- lambda-code/api-end-to-end-test/src/lib/idpClient.ts
+++ lambda-code/api-end-to-end-test/src/lib/idpClient.ts
@@ -1,17 +1,25 @@
 import { createPrivateKey } from "node:crypto";
 import { SignJWT } from "jose";
 import axios from "axios";
+import https from "https";
+
+const noSslHttpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 export class IdpClient {
+  private identityProviderTrustedDomain: string;
   private identityProviderUrl: string;
   private projectIdentifier: string;
   private privateApiKey: PrivateApiKey;
 
   public constructor(
+    identityProviderTrustedDomain: string,
     identityProviderUrl: string,
     projectIdentifier: string,
     privateApiKey: PrivateApiKey
   ) {
+    this.identityProviderTrustedDomain = identityProviderTrustedDomain;
     this.identityProviderUrl = identityProviderUrl;
     this.projectIdentifier = projectIdentifier;
     this.privateApiKey = privateApiKey;
@@ -25,7 +33,7 @@ export class IdpClient {
       .setIssuedAt()
       .setIssuer(this.privateApiKey.userId)
       .setSubject(this.privateApiKey.userId)
-      .setAudience(this.identityProviderUrl)
+      .setAudience(`https://${this.identityProviderTrustedDomain}`) // To bypass JWT Audience check from IdP
       .setExpirationTime("1 minute");
 
     return jsonWebTokenSigner
@@ -39,7 +47,9 @@ export class IdpClient {
             scope: `openid profile urn:zitadel:iam:org:project:id:${this.projectIdentifier}:aud`,
           },
           {
+            httpsAgent: noSslHttpsAgent, // To bypass self signed SSL certificate barrier
             headers: {
+              Host: this.identityProviderTrustedDomain, // To bypass HTTP Host check from IdP
               "Content-Type": "application/x-www-form-urlencoded",
             },
           }
@@ -47,7 +57,7 @@ export class IdpClient {
       )
       .then((response) => response.data.access_token as string)
       .catch((error) => {
-        throw new Error("Failed to generate access token", { cause: error });
+        throw new Error(`Failed to generate access token. Reason: ${(error as Error).message}`);
       });
   }
 }
