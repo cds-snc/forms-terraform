07dec9a4d61d1ee77e4cacae99fba3922eb8f438
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-06-21 08:36:51 -0400

    fix formatting in guard_duty file

--- aws/guard_duty/guard_duty.tf
+++ aws/guard_duty/guard_duty.tf
@@ -1,10 +1,10 @@
 
-module "guard_duty"{
-  source="github.com/cds-snc/terraform-modules//guardduty_malware_s3?ref=08f883bf79388eeef2afc01f4618c47ebb641fb4" # v10.5.1"
+module "guard_duty" {
+  source = "github.com/cds-snc/terraform-modules//guardduty_malware_s3?ref=08f883bf79388eeef2afc01f4618c47ebb641fb4" # v10.5.1"
 
 
-  s3_bucket_name     = var.reliability_storage_id
-  tagging_status     = "ENABLED"
+  s3_bucket_name = var.reliability_storage_id
+  tagging_status = "ENABLED"
 
 
   billing_tag_value = "GCForms-${var.env}"
