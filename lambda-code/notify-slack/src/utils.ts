9460a1a9694f50a5c2bc43ddca8bc338413a67ae
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-06-27 10:56:26 -0400

    instrument notify-slack

--- lambda-code/notify-slack/src/utils.ts
+++ lambda-code/notify-slack/src/utils.ts
@@ -1,3 +1,8 @@
+import AWSXRay from "aws-xray-sdk-core";
+// Need to wrap the http module globally before importing it
+// to ensure that all HTTP requests are captured by AWS X-Ray.
+AWSXRay.captureHTTPsGlobal(require("http"));
+
 import util from "util";
 import https from "https";
 import { gunzip } from "zlib";
