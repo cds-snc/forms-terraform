91c4a2d943d3b1b1f80f70bf99ed4e72dd803de9
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-07-04 14:50:41 -0400

    update provider

--- local_dev_files/upgrade_dev_env.sh
+++ local_dev_files/upgrade_dev_env.sh
@@ -3,8 +3,6 @@ SECONDS=0
 # Exit on any error
 set -e
 
-MODULE_NAME=$1
-
 # Text colors
 greenColor='\033[0;32m'
 yellowColor='\033[0;33m'
@@ -28,30 +26,23 @@ else
     exit 0
 fi
 
-if [ -z "$MODULE_NAME" ]; then
-    printf "${greenColor}=> Building All Terragrunt Modules${reset}\n"
-    terragrunt run-all init --upgrade \
-        --non-interactive --log-level info --queue-strict-include \
-        --working-dir $basedir/env \
-        --queue-include-dir $basedir/env/cloud/kms \
-        --queue-include-dir $basedir/env/cloud/network \
-        --queue-include-dir $basedir/env/cloud/secrets \
-        --queue-include-dir $basedir/env/cloud/sqs \
-        --queue-include-dir $basedir/env/cloud/s3 \
-        --queue-include-dir $basedir/env/cloud/ecr \
-        --queue-include-dir $basedir/env/cloud/sns \
-        --queue-include-dir $basedir/env/cloud/redis \
-        --queue-include-dir $basedir/env/cloud/rds \
-        --queue-include-dir $basedir/env/cloud/dynamodb \
-        --queue-include-dir $basedir/env/cloud/lambdas \
-        --queue-include-dir $basedir/env/cloud/vpn \
-        --queue-include-dir $basedir/env/cloud/guard_duty
-else
-    printf "${greenColor}=> Only building ${MODULE_NAME} Terragrunt Module${reset}\n"
-    cd $basedir/env/cloud/$MODULE_NAME
-    terragrunt apply --non-interactive --log-level info -auto-approve
-    exit 0
-fi
+printf "${greenColor}=> Building All Terragrunt Modules${reset}\n"
+terragrunt run-all init --upgrade \
+    --non-interactive --log-level info --queue-strict-include \
+    --working-dir $basedir/env \
+    --queue-include-dir $basedir/env/cloud/kms \
+    --queue-include-dir $basedir/env/cloud/network \
+    --queue-include-dir $basedir/env/cloud/secrets \
+    --queue-include-dir $basedir/env/cloud/sqs \
+    --queue-include-dir $basedir/env/cloud/s3 \
+    --queue-include-dir $basedir/env/cloud/ecr \
+    --queue-include-dir $basedir/env/cloud/sns \
+    --queue-include-dir $basedir/env/cloud/redis \
+    --queue-include-dir $basedir/env/cloud/rds \
+    --queue-include-dir $basedir/env/cloud/dynamodb \
+    --queue-include-dir $basedir/env/cloud/lambdas \
+    --queue-include-dir $basedir/env/cloud/vpn \
+    --queue-include-dir $basedir/env/cloud/guard_duty
 
 # Print out the time it took to initialize the infrastructure
 
