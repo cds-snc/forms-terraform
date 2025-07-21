8ff177c6f6a9e761dd91234f4e2f62c20ef28ef1
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2025-07-21 11:42:18 -0400

    fix typo

--- aws/app/ecs_iam.tf
+++ aws/app/ecs_iam.tf
@@ -205,7 +205,7 @@ data "aws_iam_policy_document" "forms_dynamodb" {
     ]
 
     resources = [
-      var.dynamodb_relability_queue_arn,
+      var.dynamodb_reliability_queue_arn,
       var.dynamodb_vault_arn,
       "${var.dynamodb_vault_arn}/index/*"
     ]
