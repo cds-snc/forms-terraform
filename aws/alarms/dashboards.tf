8f8afc28657f128cb660e49c2e606ba9bb847a16
Clément Janin <clement.janin@cds-snc.ca>
2025-08-11 13:32:40 -0400

    fix: reference to alarm_reliability_deadletter_queue arn

--- aws/alarms/dashboards.tf
+++ aws/alarms/dashboards.tf
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_dashboard" "forms_service_health" {
     alarm_lb_healthy_host_count             = aws_cloudwatch_metric_alarm.ELB_healthy_hosts.arn,
     alarm_lb_unhealth_host_count_tg1        = var.unhealthy_host_count_for_target_group_1_alarm_arn,
     alarm_lb_unhealth_host_count_tg2        = var.unhealthy_host_count_for_target_group_2_alarm_arn,
-    alarm_reliability_deadletter_queue      = aws_cloudwatch_metric_alarm.reliability_dead_letter_queue_warn.arn,
+    alarm_reliability_deadletter_queue      = aws_cloudwatch_metric_alarm.dlq_message_detector["reliability"].arn,
     lb_arn_suffix                           = var.lb_arn_suffix,
     ecs_cloudwatch_log_group_name           = var.ecs_cloudwatch_log_group_name,
     ecs_cluster_name                        = var.ecs_cluster_name,
