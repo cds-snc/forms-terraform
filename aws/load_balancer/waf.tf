35f4fb262866b550e7d1d3754b9f3bb99bdec5bb
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2025-09-02 09:06:52 -0400

    chore: update waf ip block version (#1087)

--- aws/load_balancer/waf.tf
+++ aws/load_balancer/waf.tf
@@ -668,7 +668,7 @@ resource "aws_wafv2_regex_pattern_set" "valid_maintenance_mode_uri_paths" {
 # that crosses a block threshold will be added to the blocklist.
 #
 module "waf_ip_blocklist" {
-  source = "github.com/cds-snc/terraform-modules//waf_ip_blocklist?ref=7341e0355a0a346574828da97863af2f552d05ed" # v10.4.6
+  source = "github.com/cds-snc/terraform-modules//waf_ip_blocklist?ref=10bb6f8d05df94698a93aa016618fd1ab22b8b2c" # v10.7.0
 
   service_name                     = "forms_app"
   athena_database_name             = "access_logs"
