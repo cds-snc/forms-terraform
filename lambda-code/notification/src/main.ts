e3752cfd576b405298eaceeee0eba2e05bbcd5bd
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-11-27 15:37:23 -0500

    Update comments

--- lambda-code/notification/src/main.ts
+++ lambda-code/notification/src/main.ts
@@ -3,24 +3,24 @@ import { sendNotification } from '@lib/email.js';
 import { createNotification, getNotification } from '@lib/db.js';
 
 /**
+ * SQS Lambda handler for sending email notifications.
+ * 
  * Three cases:
  * - Send immediately: pass emails, subject, body (no notificationId)
  * - Queue up to send when process is complete: pass notificationId, emails, subject, body
  * - Queued notification process complete, send it: pass notificationId
- * 
- * @param event 
- * @returns 
+ *
+ * @param event - SQS event containing notification messages
+ * @param event.Records[].body - JSON string with the following structure:
+ * @param event.Records[].body.notificationId - (optional) ID to defer sending until another process completes
+ * @param event.Records[].body.emails - Array of email addresses to send the notification to
+ * @param event.Records[].body.subject - Email subject line
+ * @param event.Records[].body.body - Email body content (should be pre-formatted for email delivery)
  */
 export const handler: SQSHandler = async (event) => {
   const records = event.Records;
   try {
     for (const record of records) {
-      /**
-       * notificationId - optional ID of notification to defer sending until another process has completed
-       * emails - array of 1 or more email addresses to send the notification to
-       * subject - email subject of the notification
-       * body - email body and sent in email as is, formatting should be handled before calling the lambda
-       */
       const {notificationId, emails, subject, body} = JSON.parse(record.body);
       
       // TEMP
@@ -35,7 +35,7 @@ export const handler: SQSHandler = async (event) => {
       // Case 2: defer notification and send after another proces has finished
       const notification = await getNotification(notificationId);
 
-      // New notification, store for later
+      // New notification, create and store for later
       if (!notification) {
         await createNotification(notificationId, emails, subject, body);
         return;
