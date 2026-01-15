ec43a2595c129e46e709bfb4d2297b0a5dee082f
Bryan Robitaille <bryan.robitaille@cds-snc.ca>
2026-01-15 11:17:58 -0500

    fix: idp user portal blue green deployment (#1202)

--- aws/idp/ecs_login.tf
+++ aws/idp/ecs_login.tf
@@ -25,11 +25,17 @@ locals {
     environment = [{
       "name"  = "ZITADEL_API_URL",
       "value" = "http://zitadel.${var.service_discovery_private_dns_namespace_ecs_local_name}:8080"
+
+      },
+      {
+        "name"  = "CUSTOM_REQUEST_HEADERS"
+        "value" = "Host:${local.idp_domains[0]}"
     }]
     secrets = [{
       "name"      = "ZITADEL_SERVICE_USER_TOKEN"
       "valueFrom" = aws_ssm_parameter.idp_login_service_user_token.arn
     }],
+
 
   }]
 }
