2532ba8a3272e06d19b84937da9967cdb58626c9
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2026-01-27 12:47:05 -0500

    only output old zones

--- aws/hosted_zone/outputs.tf
+++ aws/hosted_zone/outputs.tf
@@ -1,9 +1,9 @@
 output "hosted_zone_ids" {
   description = "Route53 hosted zone ID"
-  value       = aws_route53_zone.form_viewer.*.zone_id
+  value       = slice(aws_route53_zone.form_viewer.*.zone_id, length(aws_route53_zone.form_viewer.*.zone_id) > 1 ? 1 : 0, length(aws_route53_zone.form_viewer.*.zone_id))
 }
 
 output "hosted_zone_names" {
   description = "Route53 hosted zone name"
-  value       = aws_route53_zone.form_viewer.*.name
+  value       = slice(aws_route53_zone.form_viewer.*.name, length(aws_route53_zone.form_viewer.*.zone_id) > 1 ? 1 : 0, length(aws_route53_zone.form_viewer.*.zone_id))
 }
