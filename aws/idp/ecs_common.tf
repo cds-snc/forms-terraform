63a128dc4076a07a9c128284648e5b4402ff2f8a
Clément JANIN <clement.janin@cds-snc.ca>
2026-05-11 13:26:41 -0400

    chore: delete legacy IDP user portal (#1324)

--- aws/idp/ecs_common.tf
+++ aws/idp/ecs_common.tf
@@ -1,6 +1,7 @@
 #
 # ECS Cluster
-# Fargate cluster that runs the IDP and User Portal
+# Fargate cluster that runs the GC Forms IDP
+# Note: this cluster exist because we used to run an SSO user portal in it
 #
 resource "aws_ecs_cluster" "idp" {
   name = "auth"
