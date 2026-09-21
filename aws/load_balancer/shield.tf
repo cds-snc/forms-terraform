8d4e71fcd3ef0087b87e13d6ff115e7bf639aa77
Clément Janin <clement.janin@cds-snc.ca>
2026-09-21 10:31:23 -0400

    chore: enable AWS Shield DDOS protection on the GC Forms load balancer

--- aws/load_balancer/shield.tf
+++ aws/load_balancer/shield.tf
@@ -1,8 +1,25 @@
+resource "aws_shield_subscription" "forms" {
+  auto_renew = "ENABLED"
+}
+
 resource "aws_shield_protection" "alb" {
   name         = "LoadBalancer"
   resource_arn = aws_lb.form_viewer.arn
 
   tags = var.core_tags
+}
+
+resource "aws_shield_protection_health_check_association" "forms" {
+  shield_protection_id = aws_shield_protection.alb.id
+  health_check_arn     = aws_route53_health_check.lb_web_app_global_target_group.arn
+}
+
+resource "aws_shield_application_layer_automatic_response" "forms" {
+  resource_arn = aws_lb.form_viewer.arn
+
+  // AWS best practices: "Enable automatic mitigation in Count mode until Shield Advanced has established a baseline for normal, historic traffic. Shield Advanced needs from 24 hours to 30 days to establish a baseline."
+  // We can switch to `BLOCK` once this code has been in Production for a month.
+  action = "COUNT"
 }
 
 resource "aws_shield_protection" "route53_hosted_zone" {
--- aws/load_balancer/waf.tf
+++ aws/load_balancer/waf.tf
@@ -568,6 +568,42 @@ resource "aws_wafv2_web_acl" "forms_acl" {
     }
   }
 
+  rule {
+    name     = "AWSManagedRulesAntiDDoSRuleSet"
+    priority = 12
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAntiDDoSRuleSet"
+        vendor_name = "AWS"
+
+        managed_rule_group_configs {
+          aws_managed_rules_anti_ddos_rule_set {
+            client_side_action_config {
+              challenge {
+                sensitivity     = "HIGH"
+                usage_of_action = "ENABLED"
+                exempt_uri_regular_expression {
+                  regex_string = "/api/|.(acc|avi|css|gif|jpe?g|js|pdf|png|tiff?|ttf|webm|webp|woff2?)$"
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAntiDDoSRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
   // ACL visibility Metric
   visibility_config {
     cloudwatch_metrics_enabled = true
